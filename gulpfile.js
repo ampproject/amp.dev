7c8d1f627acb61a32c6dbf38e3a3f9e86c23a07a
Paul Bakaus <pbakaus@google.com>
2016-08-10 12:26:08 -0700

    added Service Worker, removed unneeded assets and optimized others

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var plumber = require('gulp-plumber');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
+var swPrecache = require('sw-precache');
+var gutil = require('gulp-util');
 
 var Path = {
   CSS_SOURCES: './assets/sass/**/*.scss',
@@ -26,6 +28,31 @@ gulp.task('update-blog-links', function () {
     }
     console.log(stdout);
   });
+});
+
+gulp.task('write-service-worker', function (callback) {
+
+  swPrecache.write('build/service-worker.js', {
+    staticFileGlobs: [
+      'build/**/*.{png,jpg,gif,svg,json}',
+      'build/*.html',
+      'build/docs/**/*.html',
+      'build/who/*.html',
+      'build/roadmap/*.html',
+      'build/metrics/*.html',
+      'build/how-it-works/*.html',
+      'build/case-studies/*.html'
+    ],
+    runtimeCaching: [{
+      urlPattern: /^https:\/\/cdn\.ampproject\.org/,
+      handler: 'fastest'
+    }],
+    stripPrefixMulti: {
+      'build/': ''
+    },
+    logger: gutil.log
+  }, callback);
+
 });
 
 gulp.task('sass', function() {
