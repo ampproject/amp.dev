aeb633b2b34c40c16dd436f13a20382bd19c46c7
Matthias Rohmer <matthias.rohmer@jvm.de>
2018-07-26 10:56:34 +0200

    ✨ Add needed bootstrap for styles.
    Setup basic task and gulp boilerplate, add new packages to package.json #1

---
+++ frontend/setup/settings.js
@@ -0,0 +1,21 @@
+/**
+ * These settings are meant to define sensible defaults for each of the tasks
+ * defined inside ~setup/tasks/. This pattern provides the ability to easily
+ * change settings based on environments and flags.
+ * @type {Object}
+ */
+const settings = {
+    'styles': {
+        'src': 'source/scss/bundles/*.scss',
+        'dest': [
+            './build/assets/css/',
+        ],
+        'options': {
+            'outputStyle': 'compressed'
+        }
+    }
+};
+
+// TODO: Maybe overwrite settings for other environments.
+
+export default settings;
