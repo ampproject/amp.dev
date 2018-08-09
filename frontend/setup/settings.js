fba42556b19c3fd1a3cad09edc005feb36e1ce21
Matthias Rohmer <rohmer.matthias@anyvent.org>
2018-08-09 22:57:04 +0200

    🐧 Use import instead of include to inline styles.
    include was getting cached and not showing changes.

--- frontend/setup/settings.js
+++ frontend/setup/settings.js
@@ -11,7 +11,7 @@ const settings = {
             './static/css/',
         ],
         'options': {
-            'outputStyle': 'expanded'
+            'outputStyle': 'compact'
         }
     },
     'templates': {
