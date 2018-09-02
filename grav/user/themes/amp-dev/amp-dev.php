b4dcb0e3f5a2b1eeccc7448fad3e0942a85fc85b
Matthias Rohmer <rohmer.matthias@anyvent.org>
2018-09-02 22:54:43 +0200

    ♻️ Only check if inAdmin once, use autoloader ...
     #63

--- grav/user/themes/amp-dev/amp-dev.php
+++ grav/user/themes/amp-dev/amp-dev.php
@@ -4,6 +4,7 @@ namespace Grav\Theme;
 use Grav\Common\Theme;
 use Grav\Theme\AmpDev\Assets;
 use Grav\Theme\AmpDev\Icons;
+use Grav\Theme\AmpDev\Markdown;
 use RocketTheme\Toolbox\Event\Event;
 
 class AmpDev extends Theme
@@ -11,53 +12,68 @@ class AmpDev extends Theme
 
   /**
    * The themes dediacted asset manager
-   * @var [type]
+   * @var Grav\Theme\AmpDev\Assets
    */
   protected $assets;
 
   /**
    * The icons utility
-   * @var [type]
+   * @var Grav\Theme\AmpDev\Icons;
    */
   protected $icons;
 
   public static function getSubscribedEvents() {
-    require_once(__DIR__.'/classes/Assets.php');
-    require_once(__DIR__.'/classes/Icons.php');
 
     return [
-      'onAssetsInitialized' => ['onAssetsInitialized', 0],
-      'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
-      'onOutputGenerated' => ['onOutputGenerated', 0],
+      'onThemeInitialized' => ['onThemeInitialized', 0],
     ];
   }
 
-  public function onAssetsInitialized($event) {
-    // Replace native assets manager with the AMP asset manager but only if not
-    // in admin as it would discard registered assets
+  /**
+   * All of the event hooks only shall take place when we are not in admin
+   * as some of the hooks may break admin functionality, like replacing
+   * the asset manager for example
+   */
+  public function onThemeInitialized() {
     if (!$this->isAdmin()) {
-      $assets = new Assets();
+      require_once(__DIR__.'/vendor/autoload.php');
 
-      $this->grav['assets'] = $assets;
-      $this->assets = $assets;
+      $this->enable([
+        'onAssetsInitialized' => ['onAssetsInitialized', 0],
+        'onMarkdownInitialized' => ['onMarkdownInitialized', 0],
+        'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+        'onOutputGenerated' => ['onOutputGenerated', 0],
+      ]);
     }
   }
 
-  public function onTwigSiteVariables($event) {
+  public function onAssetsInitialized(Event $event) {
+    $this->assets = new Assets();
+
+    // Replace native assets manager with the custom AMP asset manager that
+    // offers some helper methods to create valid AMP pages
+    $this->grav['assets'] = $this->assets;
+  }
+
+  public function onMarkdownInitialized(Event $event) {
+    // Extend Parsedown to handle custom BBCodes and class additions to default
+    // markdown elements
+    $markdown = $event['markdown'];
+    Markdown::extend($markdown);
+  }
+
+  public function onTwigSiteVariables(Event $event) {
     // Register icon utility to easily add icons
-    if (!$this->isAdmin()) {
-      $icons = new Icons();
+    $this->icons = new Icons();
 
-      $this->grav['twig']->twig_vars['icons'] = $icons;
-      $this->icons = $icons;
-    }
+    $this->grav['twig']->twig_vars['icons'] = $this->icons;
   }
 
-  public function onOutputGenerated($event) {
-    if (!$this->isAdmin()) {
-      $this->grav->output = $this->icons->definitions(false);
-      $this->grav->output = $this->assets->ampComponents(false);
-    }
+  public function onOutputGenerated(Event $event) {
+    // Lazily evaluate all assets that may have been registered during
+    // template rendering and/or plugin execution
+    $this->grav->output = $this->icons->definitions(false);
+    $this->grav->output = $this->assets->ampComponents(false);
   }
 
 }
