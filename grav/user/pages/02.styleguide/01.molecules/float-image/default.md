e9b957669d0af0aa3b804d8525d92c3ed031c4b1
Matthias Rohmer <matthias.rohmer@jvm.de>
2018-09-11 15:27:42 +0200

    ✨ Introduce second shortcode [stage].
     #63

--- grav/user/pages/02.styleguide/01.molecules/float-image/default.md
+++ grav/user/pages/02.styleguide/01.molecules/float-image/default.md
@@ -3,6 +3,11 @@ title: 'Float Image'
 media_order: 'amp-img-01.png,amp-img-02.png'
 ---
 
+[stage color=orange]
+### What is AMP?
+# AMP is a web component framework for easily creating user first websites
+[/stage]
+
 # Float Image
 ## Longo ist doof
 A float image consists of two images that are getting stacked on top of each other.
---
+++ grav/user/themes/amp-dev/classes/shortcodes/StageShortcode.php
@@ -0,0 +1,23 @@
+<?php
+namespace Grav\Plugin\Shortcodes;
+
+use Grav\Common\Grav;
+use Masterminds\HTML5;
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class StageShortcode extends Shortcode {
+
+  const TEMPLATE_PATH = 'partials/shortcodes/stage.html.twig';
+
+  public function init() {
+    $this->shortcode->getHandlers()->add('stage', function(ShortcodeInterface $sc) {
+
+      $output = $this->twig->processTemplate(self::TEMPLATE_PATH, [
+        'color' => $sc->getParameter('color'),
+        'content' => $sc->getContent()
+      ]);
+
+      return $output;
+    });
+  }
+}
