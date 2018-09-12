4c6130448e614308efef7407f506b2d1a507c9a6
andmckvr13 <andmckvr@gmail.com>
2018-09-12 15:58:03 +0200

    Revert "✨ slow down animation"
    This reverts commit a4c754dc0e89dd6bfd0ba97c8044ec1ccdc13f52.

--- grav/cache/compiled/files/77dabe86249bffc7eebe7e049ed86a81.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/andrew.mckeever/amp.dev/grav/user/config/streams.yaml',
-    'modified' => 1536757975,
-    'data' => [
-        
-    ]
-];
--- grav/cache/compiled/files/8d77cbcf552d2e8db5573e2e6a7aed41.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'plugins://devtools/devtools.yaml',
-    'modified' => 1536656622,
-    'data' => [
-        'enabled' => true
-    ]
-];
