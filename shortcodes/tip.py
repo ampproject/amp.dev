def36dcf87679d440921e189afb18f4bd234a9dc
Matthias Rohmer <matthias.rohmer@jvm.de>
2018-11-22 13:14:36 +0100

    ✨ Allow various types for tip.

--- shortcodes/tip.py
+++ shortcodes/tip.py
@@ -1,9 +1,25 @@
 from shortcodes import Shortcode
 
+ALLOWED_TYPES = ['read-on', 'important', 'note']
 
 class TipShortcode(Shortcode):
-    name = 'tip'
-    prerender_markdown = True
-    template = 'partials/tip.j2'
+  name = 'tip'
+  prerender_markdown = True
+  template = 'partials/tip.j2'
+
+  def transform(self, value, options):
+    # Only the first option key might define the type
+    options = options.items()
+    try:
+      type = options[0][0]
+    except IndexError as error:
+      type = None
+
+    if type not in ALLOWED_TYPES:
+      type = 'default'
+
+    self.context['type'] = type
+
+    return value
 
 shortcode = TipShortcode
