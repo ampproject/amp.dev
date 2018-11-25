8e2bef67edb1f99dd01186997994f50779d9e76a
Matthias Rohmer <matthias.rohmer@jvm.de>
2018-11-25 15:41:36 +0100

    🐛 Fix various bugs in stage and clean up code.

--- shortcodes/stage.py
+++ shortcodes/stage.py
@@ -1,8 +1,11 @@
 import lxml.html
 from shortcodes import Shortcode
 
+# Used to pluck out the destination switch to put it in between headlines
 DESTINATION_SWITCH_CLASSNAME = '.ad-m-destination-switch'
 
+# Allowed values for color options
+ALLOWED_BACKGROUND_COLORS = ['dark-blue', 'light-blue', 'orange', 'purple', 'green',]
 
 class StageShortcode(Shortcode):
     name = 'stage'
@@ -10,39 +13,64 @@ class StageShortcode(Shortcode):
     prerender_markdown = True
     template = 'partials/stage.j2'
 
-    # if stage includes a subheadline, retrieve and return it #
+    # If stage includes a subheadline, retrieve and return it
     def _get_sub_headline(self, dom):
-      sub_headline = dom.cssselect('h2')
-      if len(sub_headline):
-          return sub_headline[0].text_content()
-      return ''
+        sub_headline = dom.cssselect('h2')
+        if len(sub_headline):
+            return sub_headline[0].text_content()
+        return ''
 
     # if stage includes a headline, retrieve and return it #
     def _get_headline(self, dom):
-      headline = dom.cssselect('h1')
+        headline = dom.cssselect('h1')
 
-      # some stages inline the destination-switch between two headlines, check if there is more than one headline #
-      if len(headline) == 2:
-        self.context['headline_part_two'] = headline[1].text_content()
-        return headline[0].text_content()
+        # Some stages inline the destination-switch between two headlines,
+        # check if there is more than one headline
+        if len(headline) == 2:
+            self.context['headline_part_two'] = headline[1].text_content()
+            return headline[0].text_content()
 
-      elif len(headline) == 1:
-        # reset headlineTwo to empty string #
-        self.context['headline_part_two'] = ''
-        return headline[0].text_content()
+        elif len(headline) == 1:
+            # Reset headlineTwo to empty string
+            self.context['headline_part_two'] = ''
+            return headline[0].text_content()
 
-      # if no headline given, reset headlineTwo if necessary and return nothing #
-      else:
-        self.context['headline_part_two'] = ''
+        # If no headline given, reset headlineTwo if necessary and return nothing
+        else:
+            self.context['headline_part_two'] = ''
+            return ''
+
         return ''
 
-      return ''
-
     def _get_destination_switch(self, dom):
-      destination_switch = dom.cssselect(DESTINATION_SWITCH_CLASSNAME)
-      if len(destination_switch):
-          return lxml.etree.tostring(destination_switch[0])
-      return ''
+        destination_switch = dom.cssselect(DESTINATION_SWITCH_CLASSNAME)
+        if len(destination_switch):
+            html = lxml.etree.tostring(destination_switch[0])
+            # Also remove destination switch from dom to easily pluck out
+            # a CTA if there is one
+            destination_switch[0].clear()
+            return html
+        return None
+
+    def _get_background_color(self, options):
+        background_color = options.get('color', None)
+        if background_color not in ALLOWED_BACKGROUND_COLORS:
+            background_color = ALLOWED_BACKGROUND_COLORS[0]
+        return background_color
+
+    def _get_button(self, dom):
+        # Destinations will be inside a list - so only elements outside
+        # the list should taken into consideration as button
+        anchor = dom.cssselect('a:last-of-type')
+        print(anchor)
+        if len(anchor):
+          print(anchor[0].getparent())
+          return {
+            'link': anchor[0].attrib.get('href'),
+            'text': anchor[0].text_content()
+          }
+        else:
+          return {}
 
     def transform(self, value, options):
         dom = lxml.html.fromstring('<html>{}</html>'.format(value))
@@ -51,27 +79,13 @@ class StageShortcode(Shortcode):
         self.context['headline'] = self._get_headline(dom)
         self.context['destination_switch'] = self._get_destination_switch(dom)
 
+        # Only try to get button after destination switch has been plucked
+        # out as it also contains links that might interfer
+        self.context['button'] = self._get_button(dom)
 
-        anchorTag = dom.cssselect('a')
-
-        # checks if stage should include a button #
-        if len(anchorTag):
-          href = anchorTag[0].attrib.get('href')
-          if(self._pod.file_exists(href)):
-            doc = self._pod.get_doc(href)
-            self.context['button_link'] = href
-            self.context['button_title'] = anchorTag[0].text_content()
-
-        else:
-          self.context['button_link'] = ''
-          self.context['button_title'] = ''
-
-        if len(options):
-          self.context['stage_background_color'] = options.values()[0]
-        else:
-          # if no color is specified, default to blue #
-          self.context['stage_background_color'] = 'blue'
+        self.context['background_color'] = self._get_background_color(options)
 
         return value
+
 
 shortcode = StageShortcode
