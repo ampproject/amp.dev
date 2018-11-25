753e0bb381237cce5949fe232cdfc2907882e460
Matthias Rohmer <matthias.rohmer@jvm.de>
2018-11-25 17:35:41 +0100

    🔥 Remove unused sidebar shortcode.

--- shortcodes/sidebar.py
+++
@@ -1,9 +0,0 @@
-from shortcodes import Shortcode
-
-
-class SidebarShortcode(Shortcode):
-    name = 'sidebar'
-    prerender_markdown = True
-    template = 'partials/sidebar.j2'
-
-shortcode = SidebarShortcode
