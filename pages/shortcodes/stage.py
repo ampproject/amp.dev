45b35c2cd7f7f7dd80dc571d5aafba63f2864cfd
keevkeev <andmckvr@gmail.com>
2018-12-01 17:34:17 +0100

    ♻️ refactored stage to no longer accept dest_switch
    Destination switch is deprecated and should no longer be referenced inside the stage shortcode.

--- pages/shortcodes/stage.py
+++ pages/shortcodes/stage.py
@@ -1,8 +1,8 @@
-# import lxml.html
+from xml.dom import minidom
+
 from shortcodes import Shortcode
 
 # Used to pluck out the destination switch to put it in between headlines
-DESTINATION_SWITCH_CLASSNAME = '.ad-m-destination-switch'
 
 # Allowed values for color options
 ALLOWED_BACKGROUND_COLORS = ['dark-blue', 'light-blue', 'orange', 'purple', 'green',]
@@ -15,42 +15,21 @@ class StageShortcode(Shortcode):
 
     # If stage includes a subheadline, retrieve and return it
     def _get_sub_headline(self, dom):
-        sub_headline = dom.cssselect('h2')
+        sub_headline = dom.getElementsByTagName('h2')
+
         if len(sub_headline):
-            return sub_headline[0].text_content()
+            return sub_headline[0].childNodes[0].nodeValue
+
         return ''
 
     # if stage includes a headline, retrieve and return it #
     def _get_headline(self, dom):
-        headline = dom.cssselect('h1')
+        headline = dom.getElementsByTagName('h1')
 
-        # Some stages inline the destination-switch between two headlines,
-        # check if there is more than one headline
-        if len(headline) == 2:
-            self.context['headline_part_two'] = headline[1].text_content()
-            return headline[0].text_content()
-
-        elif len(headline) == 1:
-            # Reset headlineTwo to empty string
-            self.context['headline_part_two'] = ''
-            return headline[0].text_content()
-
-        # If no headline given, reset headlineTwo if necessary and return nothing
-        else:
-            self.context['headline_part_two'] = ''
-            return ''
+        if len(headline):
+            return headline[0].childNodes[0].nodeValue
 
         return ''
-
-    def _get_destination_switch(self, dom):
-        destination_switch = dom.cssselect(DESTINATION_SWITCH_CLASSNAME)
-        if len(destination_switch):
-            # html = lxml.etree.tostring(destination_switch[0])
-            # Also remove destination switch from dom to easily pluck out
-            # a CTA if there is one
-            destination_switch[0].clear()
-            return html
-        return None
 
     def _get_background_color(self, options):
         background_color = options.get('color', None)
@@ -61,34 +40,32 @@ class StageShortcode(Shortcode):
     def _get_button(self, dom):
         # Destinations will be inside a list - so only elements outside
         # the list should taken into consideration as button
-        anchor = dom.cssselect('a:last-of-type')
+        anchor = dom.getElementsByTagName('a:last-of-type')
         if len(anchor):
           return {
-            'link': anchor[0].attrib.get('href'),
-            'text': anchor[0].text_content()
+            'link': anchor[0].getAttribute('href'),
+            'text': anchor[0].childNodes[0].nodeValue
           }
         else:
           return {}
 
     def _get_image(self, dom):
-        image = dom.cssselect('amp-img')
+        image = dom.getElementsByTagName('amp-img')
         if len(image):
-          # html = lxml.etree.tostring(image[0])
+          html = dom.toString(image[0])
 
           return html
 
         return None
 
     def transform(self, value, options):
-        # dom = lxml.html.fromstring('<html>{}</html>'.format(value))
+        dom = minidom.parseString('<html>{}</html>'.format(value))
         # TODO: Bring back actual transforming of stage after we succesfully
         # removed the dependency to PIP
         self.template = None
-        return value
 
         self.context['sub_headline'] = self._get_sub_headline(dom)
         self.context['headline'] = self._get_headline(dom)
-        self.context['destination_switch'] = self._get_destination_switch(dom)
         self.context['image'] = self._get_image(dom)
 
         # Only try to get button after destination switch has been plucked
