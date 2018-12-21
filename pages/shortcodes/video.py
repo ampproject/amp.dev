d7ba5105375bb56ce594953bfa67a40bd259d4f4
Matthias Rohmer <matthias.rohmer@jvm.de>
2018-12-21 15:06:07 +0100

    ✨ Implement default amp-video with caption and poster
     #1316

--- pages/shortcodes/video.py
+++ pages/shortcodes/video.py
@@ -11,32 +11,22 @@ class VideoShortcode(Shortcode):
     render_empty = True
     template = 'views/partials/video.j2'
 
-    def _get_caption(self, options):
-        caption = options.get('caption', None)
-        return caption
-
-    def _get_src(self, options):
-        src = options.get('src', '')
-        return src
-
     def transform(self, value, options):
-        self.context['src'] = self._get_src(options)
-        self.context['caption'] = self._get_caption(options)
+        self.context['src'] = options.get('src', '')
+        # Always fall back to a 16:9 dimension if none is set explicitly
+        self.context['width'] = options.get('width', 16)
+        self.context['height'] = options.get('height', 9)
+
+        self.context['caption'] = options.get('caption', None)
+
+        self.context['poster'] = options.get('poster', None)
 
         if 'youtube.com' in self.context['src']:
           self.context['type'] = 'youtube'
 
           # Extract the video ID from full youtube link by RegEx
-          match = re.search(r'v=\w*', self.context['src'])
-
-          if match:
-            str = match.group()
-            youtube_id = re.split('=', str)
-
-            self.context['youtube_id'] = youtube_id[1]
-          else:
-            print('no string match found for youtube id, either one was not provided or format is incorrect')
-
+          youtube_id = re.search(r'v=(\w*)', self.context['src']).group(1)
+          self.context['youtube_id'] = youtube_id
         else:
           self.context['type'] = 'unsupported'
           self.context['src'] = self.context['src']
