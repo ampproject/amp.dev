42c2b4e601ab0f39e0677591e1653eca5f4732ae
Matthias Rohmer <matthias.rohmer@jvm.de>
2019-01-30 10:42:19 +0100

    🔥 Remove stale shortcodes

--- pages/shortcodes/intro.py
+++
@@ -1,9 +0,0 @@
-from shortcodes import Shortcode
-
-
-class IntroShortcode(Shortcode):
-    name = 'intro'
-    prerender_markdown = True
-    template = 'views/partials/intro.j2'
-
-shortcode = IntroShortcode
