bc5f0d18f3fca77db4fa4f64dcc6b7472037343d
Sebastian Benz <sbenz@google.com>
2019-02-08 10:32:13 +0100

    try explicit grow path for travis

--- platform/lib/pipeline/grow.js
+++ platform/lib/pipeline/grow.js
@@ -24,6 +24,7 @@ const path = require('path');
 const GROW_POD_PATH = '../pages';
 
 const GROW_DEFAULT_PATH = path.join(os.homedir(), './bin/grow');
+const GROW_TRAVIS_PATH = '/home/travis/.local/bin/grow';
 
 /**
  * A wrapper class to simplify interactions with a Grow process
@@ -56,6 +57,9 @@ class Grow {
     if (fs.existsSync(GROW_DEFAULT_PATH)) {
       this._log.info(`Using Grow installation from ${GROW_DEFAULT_PATH}`);
       return GROW_DEFAULT_PATH;
+    } else if (fs.existsSync(GROW_TRAVIS_PATH)) {
+      this._log.info(`Using Grow installation from ${GROW_TRAVIS_PATH}`);
+      return GROW_TRAVIS_PATH;
     } else {
       this._log.info('Using global Grow if present.');
       return 'grow';
