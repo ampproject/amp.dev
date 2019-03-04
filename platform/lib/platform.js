40cfb978c3a67e5bb232b1505dba3a279eaace17
Matthias Rohmer <matthias.rohmer@jvm.de>
2019-03-04 20:34:46 +0100

    🐛 Give config with API host to sample parser

--- platform/lib/platform.js
+++ platform/lib/platform.js
@@ -63,6 +63,7 @@ class Platform {
     const ampOptimizer = AmpOptimizerMiddleware.create({versionedRuntime: true});
     this.server.use((request, response, next) => {
       // don't optimize sample source or preview
+      console.log(/\/(?:source|preview)(\/\d*$)?/mi.test(request.path), request.path);
       if (/\/(?:source|preview)(\/\d*$)?/mi.test(request.path)) {
         next();
         return;
