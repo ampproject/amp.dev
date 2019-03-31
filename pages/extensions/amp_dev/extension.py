1d7625aea4535c9f0f363b547f81e04aec3ab391
Matthias Rohmer <rohmer.matthias@anyvent.org>
2019-03-31 21:54:12 +0200

    ⚡️ Cache extensions per document.

--- pages/extensions/amp_dev/extension.py
+++ pages/extensions/amp_dev/extension.py
@@ -49,7 +49,7 @@ class AmpDevPostRenderHook(hooks.PostRenderHook):
         content = previous_result if previous_result else original_body
 
         # Always validate documents and try to add missing extensions
-        content = self.extension.inject_extensions(content)
+        content = self.extension.inject_extensions(doc, content)
 
         return content
 
@@ -61,6 +61,7 @@ class AmpDevExtension(extensions.BaseExtension):
 
         # Initialize an object cache for template partials
         self.template_cache = pod.podcache.get_object_cache('amp_dev_template');
+        self.extensions_cache = pod.podcache.get_object_cache('amp_dev_extensions');
 
         # Expose extension directly on pod for use in templates
         setattr(pod, 'amp_dev', self)
@@ -71,7 +72,14 @@ class AmpDevExtension(extensions.BaseExtension):
         content = BlockVideo.trigger(original_body, content)
         return content
 
-    def inject_extensions(self, content):
+    def inject_extensions(self, doc, content):
+        # During build check if dependencies for this document has already
+        # been evaluated
+        script_tags = self.extensions_cache.get(doc.pod_path)
+        if script_tags and not pod.podspec.env.name == 'development':
+            content = content.replace('</head>', ''.join(script_tags))
+            return content
+
         validation_result = validate(content)['-']
         missing_extensions = []
         if 'errors' in validation_result:
@@ -82,7 +90,7 @@ class AmpDevExtension(extensions.BaseExtension):
 
         script_tags = []
         for extension in set(missing_extensions):
-            # TODO: Handle different versions, URL and type within VALID_DEPENDENCIES
+            # TODO: Handle different versions, URL and type
             src = 'https://cdn.ampproject.org/v0/{}-0.1.js'.format(extension)
             type = 'element' if extension is not 'amp-mustache' else 'template'
 
@@ -92,6 +100,9 @@ class AmpDevExtension(extensions.BaseExtension):
         # Add tags to end of <head>
         script_tags.append('</head>')
         content = content.replace('</head>', ''.join(script_tags))
+
+        # Cache extensions to use for build
+        self.extensions_cache.add(doc.pod_path, script_tags)
 
         return content
 
