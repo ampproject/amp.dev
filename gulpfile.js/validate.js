dc04613e363bb06fcda786a7641575bd8b69835e
Sebastian Benz <sbenz@google.com>
2019-04-10 09:58:34 +0200

    use constant for src dir

--- gulpfile.js/validate.js
+++ gulpfile.js/validate.js
@@ -19,12 +19,14 @@
 const {src} = require('gulp');
 const {join} = require('path');
 const gulpAmpValidator = require('gulp-amphtml-validator');
+const {GROW_BUILD_DEST} = require('@lib/utils/project').paths;
+
 
 /**
  * Validates all pages build into /platform/pages.
  */
 function validate() {
-  return src(join(__dirname, '../platform/pages/**/*.amp.html'))
+  return src(join(GROW_BUILD_DEST, '/**/*.amp.html'))
       // Validate the input and attach the validation result to the "amp" property
       // of the file object.
       .pipe(gulpAmpValidator.validate())
