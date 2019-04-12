98f761ae40712b924b4e8cc465f31ef3874499ef
Matthias Rohmer <rohmer.matthias@anyvent.org>
2019-04-12 22:11:43 +0200

    🐛 Replace index.html with root.

--- pages/extensions/amp_dev/extension.py
+++ pages/extensions/amp_dev/extension.py
@@ -9,8 +9,7 @@ class AmpDevUrl(urls._Url):
 
     def __init__(self, path, host=None, port=None, scheme=None):
         super(AmpDevUrl, self).__init__(path, host=None, port=None, scheme=None)
-        if not 'localhost' in host:
-            self.path = self.path.replace('/index.html', '').replace('.html', '')
+        self.path = self.path.replace('/index.html', '/').replace('.html', '')
 
 urls.Url = AmpDevUrl
 
