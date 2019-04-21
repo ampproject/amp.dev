c94e1377f519f21ba816e1ea9ee0a173f4ab860c
Matthias Rohmer <rohmer.matthias@anyvent.org>
2019-04-21 19:30:05 +0200

    ✨ Add timer to express.

--- platform/lib/platform.js
+++ platform/lib/platform.js
@@ -95,6 +95,19 @@ class Platform {
       res.redirect('https://' + req.hostname + req.path);
     });
 
+    // debug computing times
+    this.server.use((req, res, next) => {
+      const timeStart = process.hrtime();
+
+      res.on('finish', () => {
+        const timeElapsed = process.hrtime(timeStart);
+        const ms = timeElapsed[0] * 1000 + timeElapsed[1] / 1e6;
+        console.log(`${req.path}: ${ms}ms`);
+      });
+
+      next();
+    });
+
     // pass app engine HTTPS status to express app
     this.server.set('trust proxy', true);
     this._enableCors();
