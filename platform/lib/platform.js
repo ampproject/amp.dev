18b545aac390d7980fc32166b7492f0e75836dc4
Matthias Rohmer <rohmer.matthias@anyvent.org>
2019-04-21 19:36:04 +0200

    🔊 Add accept-encoding and tag to timing log.

--- platform/lib/platform.js
+++ platform/lib/platform.js
@@ -102,7 +102,7 @@ class Platform {
       res.on('finish', () => {
         const timeElapsed = process.hrtime(timeStart);
         const ms = timeElapsed[0] * 1000 + timeElapsed[1] / 1e6;
-        console.log(`${req.path}: ${ms}ms`);
+        console.log(`[TIMING]: ${req.path} [accept-encoding: ${req.headers['accept-encoding']}]: ${ms}ms`);
       });
 
       next();
