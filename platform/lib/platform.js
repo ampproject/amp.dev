53ebce92cec63e41f7f895154937dd8ae923779e
Sebastian Benz <sbenz@google.com>
2019-04-22 06:26:00 +0200

    log subdomains

--- platform/lib/platform.js
+++ platform/lib/platform.js
@@ -104,7 +104,21 @@ class Platform {
         let seconds = (timeElapsed[0] * 1000 + timeElapsed[1] / 1e6) / 1000;
         seconds = seconds.toFixed(3);
         const prefix = seconds > 1 ? 'CRITICAL_TIMING' : 'TIMING';
-        console.log(`[${prefix}]: ${req.url}: ${seconds}s`);
+        let url = '';
+        if (req.headers['x-forwarded-proto'] === 'https') {
+          url = 'https://';
+        } else {
+          url = 'http://';
+        }
+        if (req.subdomains) {
+          url += req.subdomains.join('.');
+        }
+        url += req.get('host');
+        if (req.port) {
+          url += ':' + req.port;
+        }
+        url += req.url;
+        console.log(`[${prefix}]: ${url}: ${seconds}s`);
       });
 
       next();
