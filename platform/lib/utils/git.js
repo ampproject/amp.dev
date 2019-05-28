f060fee32f151b9cfbd9a5712b6cb4148a441fdb
Matthias Rohmer <rohmer.matthias@anyvent.org>
2019-05-28 21:30:02 +0200

    🚸 Use last non-merge commit message for build info

--- platform/lib/utils/git.js
+++ platform/lib/utils/git.js
@@ -17,5 +17,5 @@ const {execSync} = require('child_process');
 
 
 module.exports.version = execSync('git log -1 --pretty=%H').toString().trim();
-module.exports.message = execSync('git log -1 --pretty=%B').toString().trim();
+module.exports.message = execSync('git log -1 --pretty=%B --no-merges').toString().trim();
 module.exports.user = execSync('git config user.name').toString().trim();
