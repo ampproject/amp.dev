b4ee51132c81a3a34206bbb7fe48e64c38e9a57a
Sebastian Benz <sbenz@google.com>
2019-05-28 17:15:03 +0200

    Migrate boilerplate generator to AMP SW
    Fixes #2052

--- boilerplate/lib/templates.js
+++ boilerplate/lib/templates.js
@@ -44,11 +44,6 @@ Handlebars.registerHelper('scss', (scssPath) => {
   throw new Error('File not found ' + scssPath);
 });
 
-const beautifyJs = require('js-beautify').js;
-const BEAUTIFY_OPTIONS_JS = {
-  'indent_size': 2,
-};
-
 const PARTIALS_DIR = '../partials/';
 const ICONS_DIR = '../../../frontend/icons/';
 
@@ -76,10 +71,7 @@ function readTemplate(name) {
   let string = io.readFile(name);
   string = renderTemplate(string);
   let ext = path.extname(name).substring(1);
-  if (ext === 'html') {
-    // string = beautifyHtml(string, BEAUTIFY_OPTIONS);
-  } else if (ext === 'js') {
-    string = beautifyJs(string, BEAUTIFY_OPTIONS_JS);
+  if (ext === 'js') {
     ext = 'javascript';
   }
   string = hljs.highlight(ext, string).value;
