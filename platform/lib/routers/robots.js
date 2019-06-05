88c58201fdd66a92bd43db98903c2d9fd07b241b
Thorsten Harders <thorsten.harders@jvm.de>
2019-06-05 10:41:03 +0200

    robots.js code cleanup after review

--- platform/lib/routers/robots.js
+++ platform/lib/routers/robots.js
@@ -16,15 +16,17 @@
 
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const {setMaxAge} = require('@lib/utils/cacheHelpers');
 const project = require('@lib/utils/project');
 const config = require('@lib/config');
 
+
 function createRobotsHandler(robotsFile) {
   // eslint-disable-next-line new-cap
   const router = express.Router();
-  const robotsFilePath = config.isProdMode() ? 'robots/' + robotsFile : 'robots/disallow_all.txt';
+  const robotsFilePath = path.join('robots', config.isProdMode() ? robotsFile : 'disallow_all.txt');
   router.get('/robots.txt', (request, response) => {
     setMaxAge(response, 60 * 60);
     response.status(200)
