f1572ec8589bfdfc184168b40ce2a8556f7898c7
Matthias Rohmer <matthias.rohmer@jvm.de>
2019-07-10 19:40:15 +0200

    ✅ Adjust tests to new router.

--- platform/lib/routers/pages.test.js
+++ platform/lib/routers/pages.test.js
@@ -1,21 +1,11 @@
+const URL = require('url').URL;
 const express = require('express');
 const request = require('supertest');
-
-// We want to unit test the prod mode
-jest.setMock('@lib/config', {
-  isDevMode: () => {
-    return false;
-  },
-});
+const config = require('@lib/config');
 
 const app = express();
 const router = require('./pages.js');
 app.use(router);
-
-// We use non existing urls so the static handler used by page.js will call our next router
-// The next router remembers the request, so we can then test the url rewriting
-
-let lastRequest = undefined;
 
 // eslint-disable-next-line new-cap
 const next = express.Router();
@@ -26,111 +16,52 @@ next.get('/*', async (req, res) => {
 
 app.use(next);
 
-beforeEach(() => {
-  lastRequest = undefined;
-});
+function url(path) {
+  return new URL(path, config.hosts.platform.base).toString()
+}
 
-test('Url without extension is rewritten to .html at the end', (done) => {
-  request(app)
-      .get('/nonexisting/file')
-      .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.html');
-      })
-      .end(done);
-});
-
-test('Url with html extension is not rewritten to extra .html at the end', (done) => {
-  request(app)
-      .get('/nonexisting/file.html')
-      .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.html');
-      })
-      .end(done);
-});
-
-test('Url with xml extension is not rewritten to .html at the end', (done) => {
-  request(app)
-      .get('/nonexisting/file.xml')
-      .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.xml');
-      })
-      .end(done);
-});
-
-test('Url with / at the end is rewritten to index.html at the end', (done) => {
+test('URL with / at the end is not redirected', (done) => {
   request(app)
       .get('/nonexisting/')
       .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/index.html');
-      })
       .end(done);
 });
 
-test('Url with format website is not rewritten', (done) => {
+test('URL without extension gets trailing slash', (done) => {
   request(app)
-      .get('/nonexisting/file.html?format=website')
+      .get('/nonexisting/file')
+      .expect(301)
+      .expect('Location', url('/nonexisting/file/'))
+      .end(done);
+});
+
+test('URL with double / at the end gets one removed', (done) => {
+  request(app)
+      .get('/nonexisting//')
+      .expect(301)
+      .expect('Location', url('/nonexisting/'))
+      .end(done);
+});
+
+test('URL with .html extension gets .html removed and trailing slash added', (done) => {
+  request(app)
+      .get('/nonexisting/file.html')
+      .expect(301)
+      .expect('Location', url('/nonexisting/file/'))
+      .end(done);
+});
+
+test('URL with .amp.html extension gets .amp.html removed and trailing slash added', (done) => {
+  request(app)
+      .get('/nonexisting/file.amp.html')
+      .expect(301)
+      .expect('Location', url('/nonexisting/file/'))
+      .end(done);
+});
+
+test('URL with .js extension is not rewritten', (done) => {
+  request(app)
+      .get('/nonexisting/file.js')
       .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.html');
-      })
       .end(done);
-});
-
-
-test('Url with format ads is rewritten', (done) => {
-  request(app)
-      .get('/nonexisting/file.html?format=ads')
-      .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.ads.html');
-      })
-      .end(done);
-});
-
-test('Url with format ads is rewritten', (done) => {
-  request(app)
-      .get('/nonexisting/file.html?format=stories')
-      .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.stories.html');
-      })
-      .end(done);
-});
-
-test('Url with format email is rewritten', (done) => {
-  request(app)
-      .get('/nonexisting/file.html?format=email')
-      .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.email.html');
-      })
-      .end(done);
-});
-
-test('Url with format unknown is not rewritten', (done) => {
-  request(app)
-      .get('/nonexisting/file.html?format=unknown')
-      .expect(200, 'next')
-      .expect(() => {
-        expect(lastRequest.url).toBe('/nonexisting/file.html');
-      })
-      .end(done);
-});
-
-test('redirect index.amp.html to folder', (done) => {
-  request(app)
-      .get('/someurl/index.amp.html')
-      .expect('Location', '/someurl/')
-      .expect(302, done);
-});
-
-test('redirect file.amp.html to file without amp.html', (done) => {
-  request(app)
-      .get('/someurl/file.amp.html')
-      .expect('Location', '/someurl/file')
-      .expect(302, done);
 });
