8b629089844d783d17e8689bd8e536dbb660f151
Dennis Lemm <dennis.lemm@jvm.de>
2019-07-24 17:11:14 +0200

    🎨 Split "filter" regex-expression and loop over found filter with attributes

--- pages/extensions/amp_dev/markdown_extras/block_filter.py
+++ pages/extensions/amp_dev/markdown_extras/block_filter.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 import re
 
 FILTER_TRIGGER = '[filter'
-FILTER_START_TAG_PATTERN = re.compile(r'(\[filter( (formats|level)=(?:\"|\')(.*?)(?:\"|\'))?\])',
-                                   re.MULTILINE)
+FILTER_TAG_PATTERN = re.compile(r'\[filter.*?\]', re.MULTILINE)
+ATTRIBUTE_PATTERN = re.compile(r'(\w+)=(?:\"|\')(.*?)(?:\"|\')')
 FILTER_END_TAG_PATTERN = '[/filter]'
 
-DEFAULT_FORMATS = ['websites', 'ads', 'stories', 'email']
-DEFAULT_LEVEL = ['beginner', 'advanced']
+DEFAULT_FORMATS = 'websites, ads, stories, email'
+DEFAULT_LEVEL = 'beginner, advanced'
 
 def trigger(original_body, content):
   if FILTER_TRIGGER in original_body:
@@ -18,10 +18,27 @@ def trigger(original_body, content):
 
 
 def _transform(content):
-  for match in FILTER_START_TAG_PATTERN.findall(content):
-    formats = match[3] or DEFAULT_FORMATS
-    level = match[3] or DEFAULT_LEVEL
-    content = content.replace(match[0], '{% call filter(formats=\'' + match[3] + '\' + level=\'' + match[3] + '\') %}\n')
-  # Then also replace end tags
-  content = content.replace(FILTER_END_TAG_PATTERN, '\n{% endcall %}')
-  return content
+    for match in FILTER_TAG_PATTERN.findall(content):
+        #Match attribures from filter tag
+        attributes = _get_attributes(match)
+
+        #prepare replacement
+        replacement = '{% call filter(formats=\'' + attributes['formats'] + '\' , level=\'' + attributes['level'] + '\') %}\n'
+
+        # Then also replace end tags
+        content = content.replace(FILTER_END_TAG_PATTERN, '\n{% endcall %}')
+
+        content = content.replace(match, replacement)
+
+    return content
+
+def _get_attributes(match):
+    matches = ATTRIBUTE_PATTERN.findall(match)
+    # Set reasonable defaults
+    attributes = {
+    'formats': DEFAULT_FORMATS,
+    'level': DEFAULT_LEVEL
+    }
+    for match in matches:
+        attributes[match[0]] = match[1]
+    return attributes
