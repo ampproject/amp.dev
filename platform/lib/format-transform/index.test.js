eaf2cdd10ffe9af597b7c641b0f97667f14e0b6d
Filip Stanis <fstanis@google.com>
2019-08-29 23:30:14 +0100

    add tests

--- platform/lib/format-transform/index.test.js
+++ platform/lib/format-transform/index.test.js
@@ -14,38 +14,120 @@
  * limitations under the License.
  */
 
-const formatTransform = require('./index');
+const {getInstance} = require('./index');
 
 const PLATFORM_HOST = require('../config.js').hosts.platform.base;
 
 describe('formatTransform', () => {
+  let formatTransform = null;
+
+  beforeEach(async (done) => {
+    if (!formatTransform) {
+      formatTransform = await getInstance();
+    }
+    done();
+  });
+
+  it('makes no changes when target is websites', () => {
+    const input = '<html ⚡><head></head><body></body></html>';
+    expect(formatTransform.transform(input, 'websites')).toBe(input);
+  });
+
   it('changes html tag and cleans up head', () => {
-    const input = '<html ⚡><head><link><meta><noscript></noscript></head><body></body></html>';
-    const want = '<html ⚡4email><head></head><body></body></html>';
-    expect(formatTransform.transform(input, 'email')).toBe(want);
+    const input = s(`<!doctype html>
+<html ⚡>
+<head>
+<link><meta><noscript></noscript>
+<meta charset="utf-8">
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<style amp-boilerplate></style>
+</head>
+<body></body>
+</html>`);
+    const want = s(`<!doctype html>
+<html ⚡4email>
+<head>
+<meta charset="utf-8">
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<style amp4email-boilerplate>body{visibility:hidden}</style>
+</head>
+<body></body>
+</html>`);
+    const output = s(formatTransform.transform(input, 'email'));
+    expect(output).toBe(want);
   });
 
   it('makes URLs absolute', () => {
-    const input = `<html ⚡><head></head><body>
+    const input = s(`<!doctype html><html ⚡><head><meta charset="utf-8"><script async src="https://cdn.ampproject.org/v0.js"></script><script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script><style amp-boilerplate></style></head>
+<body>
 <a href="/something">Link</a>
-<amp-img src="/something"></amp-img>
+<amp-img src="/something" width="1" height="1"></amp-img>
 <form action-xhr="/something"></form>
-</body></html>`;
-    const want = `<html ⚡4email><head></head><body>
+</body>
+</html>`);
+    const want = s(`<!doctype html><html ⚡4email><head><meta charset="utf-8"><script async src="https://cdn.ampproject.org/v0.js"></script><script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script><style amp4email-boilerplate>body{visibility:hidden}</style></head>
+<body>
 <a href="${PLATFORM_HOST}/something">Link</a>
-<amp-img src="${PLATFORM_HOST}/something"></amp-img>
+<amp-img src="${PLATFORM_HOST}/something" width="1" height="1"></amp-img>
 <form action-xhr="${PLATFORM_HOST}/something"></form>
-</body></html>`;
-    expect(formatTransform.transform(input, 'email')).toBe(want);
+</body>
+</html>`);
+    const output = s(formatTransform.transform(input, 'email'));
+    expect(output).toBe(want);
   });
 
-  it('replaces boilerplate', () => {
-    const input = `<html ⚡4email><head>
-<style amp-boilerplate></style>
-</head><body></body></html>`;
-    const want = `<html ⚡4email><head>
-<style amp4email-boilerplate>body{visibility:hidden}</style>
-</head><body></body></html>`;
-    expect(formatTransform.transform(input, 'email')).toBe(want);
+  it('removes @formats', () => {
+    const input = s(`<html ⚡><head></head>
+<!-- comment @formats(websites) -->
+<body>
+</body>
+</html>`);
+    const want = s(`<html ⚡><head></head>
+<!-- comment  -->
+<body>
+</body>
+</html>`);
+
+    const output = s(formatTransform.transform(input, 'websites'));
+    expect(output).toBe(want);
+  });
+
+  it('applies @formats filter', () => {
+    const input = s(`<!doctype html><html ⚡><head><meta charset="utf-8"><script async src="https://cdn.ampproject.org/v0.js"></script><style amp-boilerplate></style></head>
+<body>
+<!-- comment @formats(websites) -->
+<div>foo</div>
+<!-- comment -->
+<div>bar</div>
+<!-- comment @formats(email) -->
+<div>baz</div>
+</body>
+</html>`);
+    const want = s(`<!doctype html><html ⚡4email><head><meta charset="utf-8"><script async src="https://cdn.ampproject.org/v0.js"></script><style amp4email-boilerplate>body{visibility:hidden}</style></head>
+<body>
+<!-- comment -->
+<div>bar</div>
+<!-- comment  -->
+<div>baz</div>
+</body>
+</html>`);
+
+    const output = s(formatTransform.transform(input, 'email'));
+    expect(output).toBe(want);
+  });
+
+  it('returns null when result is not valid AMP', () => {
+    const input = `<!doctype html><html ⚡><head></head><body></body></html>`;
+    expect(formatTransform.transform(input, 'email')).toBe(null);
+  });
+
+  it('throws when format is not supported', () => {
+    expect(() => {
+      formatTransform.transform('<html ⚡></html>', 'foobar')
+    }).toThrow();
   });
 });
+
+function s(str) {
+  return str.replace(/\n/g, '').trim();
+}
