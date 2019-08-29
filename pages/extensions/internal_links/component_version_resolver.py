a82f63bc586294394b374c8b3424581e9d7511af
Thorsten Harders <thorsten.harders@jvm.de>
2019-08-29 16:57:19 +0200

    Ensure the grow link handling does not confuse locale

--- pages/extensions/internal_links/component_version_resolver.py
+++ pages/extensions/internal_links/component_version_resolver.py
@@ -6,8 +6,8 @@ COMPONENT_VERSION_PATTERN = re.compile('.*/(amp-.+)-v(\d+)\.(\d+)', re.IGNORECAS
 
 class ComponentDocInfo(object):
 
-  def __init__(self, doc, major_version, minor_version):
-    self.doc = doc
+  def __init__(self, pod_path, major_version, minor_version):
+    self.pod_path = pod_path
     self.major_version = int(major_version)
     self.minor_version = int(minor_version)
 
@@ -20,9 +20,9 @@ class ComponentDocInfo(object):
       return True
     return False
 
-  def update_if_older(self, doc, other_major, other_minor):
+  def update_if_older(self, pod_path, other_major, other_minor):
     if self.is_older(other_major, other_minor):
-      self.doc = doc
+      self.pod_path = pod_path
       self.major_version = int(other_major)
       self.minor_version = int(other_minor)
 
@@ -35,6 +35,7 @@ class ComponentVersionResolver(object):
     :type components_path: str
     """
     self.pod = pod
+    self.default_locale = pod.podspec.default_locale
     self.latest_components = self.init_latest_versions(components_path)
     self.no_version_component_pattern = self.init_no_version_pattern(components_path)
 
@@ -51,7 +52,7 @@ class ComponentVersionResolver(object):
     """
     result = {}
     component_docs = self.pod.get_collection(components_path)
-    for item in component_docs.list_docs():
+    for item in component_docs.list_docs(locale=self.default_locale):
       match = COMPONENT_VERSION_PATTERN.match(item.pod_path)
       if match:
         component = match.group(1)
@@ -59,16 +60,22 @@ class ComponentVersionResolver(object):
         minor_version = match.group(3)
         if component in result:
           doc_info = result[component]
-          doc_info.update_if_older(item, major_version, minor_version)
+          doc_info.update_if_older(item.pod_path, major_version, minor_version)
         else:
-          doc_info = ComponentDocInfo(item, major_version, minor_version)
+          doc_info = ComponentDocInfo(item.pod_path, major_version, minor_version)
           result[component] = doc_info
     return result
 
   def find_latest_for_component_with_no_version(self, pod_path):
+    """
+    Return the pod path to the latest version if the given pod_path points to a component
+    and has no version information.
+    :param pod_path:
+    :return:
+    """
     match = self.no_version_component_pattern.match(pod_path)
     if match:
       component = match.group(1)
       if component in self.latest_components:
-        return self.latest_components[component].doc
+        return self.latest_components[component].pod_path
     return None
