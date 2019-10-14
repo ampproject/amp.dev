33a207651b36c2d4563ffd3c4ac7689eb5e167be
Sebastian Benz <sbenz@google.com>
2019-10-14 12:10:00 +0200

    shorten tag

--- platform/lib/utils/git.js
+++ platform/lib/utils/git.js
@@ -16,6 +16,6 @@
 const {execSync} = require('child_process');
 
 
-module.exports.version = execSync('git log -1 --pretty=%H').toString().trim();
+module.exports.version = execSync('git log -1 --pretty=format:%h ').toString().trim();
 module.exports.message = execSync('git log -1 --pretty=%B --no-merges').toString().trim();
 module.exports.user = execSync('git config user.name').toString().trim();
