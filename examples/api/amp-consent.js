2995440a5f2090722ce6a83d959141e770c60d06
Micajuine Ho <micajuineho@google.com>
2019-12-09 11:39:02 -0800

    adding endpoint & opt-out example page, bugfixing

--- examples/api/amp-consent.js
+++ examples/api/amp-consent.js
@@ -26,4 +26,13 @@ examples.post('/get-consent', (request, response) => {
   });
 });
 
+examples.post('/get-consent-geo-override', (request, response) => {
+  response.json({
+    expireCache: true,
+    consentRequired: true,
+    consentStateValue: 'accepted',
+    consentString: 'abc123',
+  });
+});
+
 module.exports = examples;
