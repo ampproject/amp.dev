660046a82e5aeea6b0005ff0616d6cb26e51312a
Sebil Satici <sebil.satici@gmail.com>
2019-12-10 20:35:59 +0100

    :sparkles: Cache latest search query and results
    Imports workbox to register routes #3102

--- pages/static/serviceworker.js
+++ pages/static/serviceworker.js
@@ -1,3 +1,4 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
   assetCachingOptions: [{
@@ -9,3 +10,68 @@ AMP_SW.init({
     assets: [],
   },
 });
+
+const CACHE_NAME = 'AMP-DEV-SEARCH-CACHE';
+const LATEST_QUERY_URL = '/search/latest-query'
+
+
+// Match search request url
+const matchDo = ({url, event}) => {
+  return (url.pathname === '/search/do');
+};
+
+// Handle search request
+const handlerDo = async ({url, event, params}) => {
+  // Extract query parameter from search request url
+  // Put new query as response to AMP-DEV-SEARCH-CACHE
+  const searchQuery = url.search.match(/q=([^&]+)/)[1];
+  const cache = await caches.open(CACHE_NAME);
+
+  cache.put(LATEST_QUERY_URL, new Response(`"${searchQuery}"`));
+
+  let response = await cache.match(event.request);
+
+  // If search results are already in cache, skip
+  // Else fetch new search request and cache whole results
+  if (response) {
+    return response
+  } else {
+    response = await fetch(event.request);
+
+    if (response.status == 200) {
+      cache.put(event.request, response.clone());
+    }
+
+    return response
+  }
+};
+
+// Register new route to handle search request
+workbox.routing.registerRoute(matchDo, handlerDo);
+
+
+// Match latest query url
+const matchCq = ({url, event}) => {
+  return (url.pathname === LATEST_QUERY_URL);
+};
+
+// Handle latest query
+const handlerCq = async ({url, event, params}) => {
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    const response = await cache.match(LATEST_QUERY_URL);
+
+    // Update latest query
+    if (response) {
+      return response;
+    } else {
+      return new Response('null')
+    }
+
+  } catch(err) {
+    return new Response('null')
+  }
+};
+
+// Register new route to handle latest query
+workbox.routing.registerRoute(matchCq, handlerCq);
