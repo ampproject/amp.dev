9472b61b057055e25914300497f7a2f64f2afc38
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-05-13 23:25:00 +0200

    🚨 Fix linter warnings

--- playground/src/csp-hash-calculator/csp-hash-calculator.js
+++ playground/src/csp-hash-calculator/csp-hash-calculator.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /* eslint-disable new-cap */
-import { calculateHash } from '@ampproject/toolbox-script-csp'
+import {calculateHash} from '@ampproject/toolbox-script-csp';
 
 export function createCspHashCalculator(editor) {
   return new CspHashCalculator(editor);
@@ -46,7 +46,10 @@ class CspHashCalculator {
     // If there is no existing meta[name="amp-script-src"] create one
     // and return as this triggers a new update anyway
     if (!this.metaAmpScriptSrc.to) {
-      this.editor.replaceRange(`${this._getMetaAmpScriptSrc()}\n`, {line: this.head.to.line, ch: 0});
+      this.editor.replaceRange(`${this._getMetaAmpScriptSrc()}\n`, {
+        line: this.head.to.line,
+        ch: 0,
+      });
       this.editor.codeMirror.indentLine(this.head.to.line);
       return;
     }
@@ -56,27 +59,37 @@ class CspHashCalculator {
 
     // Check if any inline hash has updated, if no hashes are left
     // meta[name="amp-script-src"] is up to date
-    inlineHashes = inlineHashes.filter((hash) => !existingHashes.includes(hash));
+    inlineHashes = inlineHashes.filter(
+      (hash) => !existingHashes.includes(hash)
+    );
     if (!inlineHashes.length) {
       return;
     }
 
     // Now filter out hashes that have previously been added by
     // the calculator and store the newly created ones
-    existingHashes = existingHashes.filter((hash) => !this.createdHashes.includes(hash));
+    existingHashes = existingHashes.filter(
+      (hash) => !this.createdHashes.includes(hash)
+    );
     this.createdHashes = inlineHashes;
 
-
-    this.editor.replaceRange(`${this._getMetaAmpScriptSrc([...inlineHashes, ...existingHashes])}`, this.metaAmpScriptSrc.from, this.metaAmpScriptSrc.to);
+    this.editor.replaceRange(
+      `${this._getMetaAmpScriptSrc([...inlineHashes, ...existingHashes])}`,
+      this.metaAmpScriptSrc.from,
+      this.metaAmpScriptSrc.to
+    );
   }
 
   _getExistingHashes() {
-    const html = this.editor.codeMirror.getRange(this.metaAmpScriptSrc.from, this.metaAmpScriptSrc.to);
+    const html = this.editor.codeMirror.getRange(
+      this.metaAmpScriptSrc.from,
+      this.metaAmpScriptSrc.to
+    );
     return html.match(/(?<=content=").*?(?=")/gms)[0].split(' ');
   }
 
   _getMetaAmpScriptSrc(hashes = []) {
-    return `<meta name="amp-script-src" content="${hashes.join(' ')}"/>`
+    return `<meta name="amp-script-src" content="${hashes.join(' ')}"/>`;
   }
 
   _getInlineHashes() {
@@ -123,7 +136,11 @@ class CspHashCalculator {
             this.metaAmpScriptSrc.to = {line: i, ch: token.end + 1};
             // If this meta element has a name attribute with amp-script-src
             // we found our meta element, otherwise start search again
-            if (!this.editor.codeMirror.getRange(this.metaAmpScriptSrc.from, this.metaAmpScriptSrc.to).includes('name="amp-script-src"')) {
+            if (
+              !this.editor.codeMirror
+                .getRange(this.metaAmpScriptSrc.from, this.metaAmpScriptSrc.to)
+                .includes('name="amp-script-src"')
+            ) {
               this.metaAmpScriptSrc = {};
             }
           }
@@ -135,7 +152,10 @@ class CspHashCalculator {
             script.from = {line: i, ch: token.start - 1};
           } else {
             script.to = {line: i, ch: token.end + 7};
-            script.html = this.editor.codeMirror.getRange(script.from, script.to);
+            script.html = this.editor.codeMirror.getRange(
+              script.from,
+              script.to
+            );
 
             if (script.html.includes('target="amp-script"')) {
               this.inlineScripts.push(script.html);
