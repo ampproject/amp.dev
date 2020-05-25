5e03fe02efe4190fce498e93633623eefbf6cc5d
Matthias Rohmer <12857772+matthiasrohmer@users.noreply.github.com>
2020-05-25 18:18:16 +0200

    🎉  Thumbor (#3993)
    * 🎉 Initially setup thumbor
    
    * ✨ Marry AMP optimizer and Thumbor
    
    * 🚨 Fix linter errors.
    
    * 🐳 Ignore thumbor app for default app
    
    * 🔧 Give unique ID to thumbor service
    
    * 🔧 Listen to port 8080 for GCloud compatibility
    
    * 🔧 Add thumbor host for staging
    
    * 👷 Deploy thumbor for staging
    
    * 🐛 Fall back to original URL if thumbor is unreachable

---
+++ platform/lib/routers/thumbor.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const express = require('express');
+const {join} = require('path');
+const config = require('@lib/config');
+const HttpProxy = require('http-proxy');
+const log = require('@lib/utils/log')('Thumbor');
+
+const SECURITY_KEY = 'unsafe';
+
+const proxyOptions = {
+  target: config.hosts.thumbor.base,
+  changeOrigin: true,
+};
+const proxy = HttpProxy.createProxyServer(proxyOptions);
+
+// eslint-disable-next-line new-cap
+const thumborRouter = express.Router();
+
+const imagePaths = config.shared.thumbor.fileExtensions.map((extension) => {
+  return join('/static/', '/**/', `*.${extension}`);
+});
+
+thumborRouter.get(imagePaths, (request, response, next) => {
+  const imageUrl = new URL(request.url, config.hosts.platform.base);
+  const imageWidth = imageUrl.searchParams.get('width');
+  // Thumbor expects SECURITY_KEY as URL partial and the desired
+  // size (x * y) as another one
+  request.url = join(
+    SECURITY_KEY,
+    imageWidth ? `/${imageWidth}x0/` : '/',
+    imageUrl.pathname
+  );
+
+  proxy.web(request, response, proxyOptions, (error) => {
+    // Silently fail over if no thumbor instance can be reached. Therefore
+    // rewrite the URL back to the original one
+    if (e.code == 'ECONNREFUSED') {
+      request.url = imageUrl.href;
+      next();
+      return;
+    }
+
+    // Everything else is considered a error: malformed URLs,
+    // unavailable assets, ...
+    log.error(error);
+    response.status(502).end();
+  });
+});
+
+module.exports = thumborRouter;
