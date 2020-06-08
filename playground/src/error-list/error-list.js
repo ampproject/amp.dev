fe9ba46d9391ea894cbe3b8ff887235907341743
Robin Van Opstal <robin.van-opstal@jvm.de>
2020-06-08 15:58:25 +0200

    ✨ Create reusable fly-in component and adapt error-list

--- playground/src/error-list/error-list.js
+++ playground/src/error-list/error-list.js
@@ -17,19 +17,23 @@ require('./error-list.scss');
 import events from '../events/events.js';
 import * as Button from '../button/button.js';
 import * as Validator from '../validator/validator.js';
+import FlyIn from '../fly-in/base.js';
 
 export const EVENT_ERROR_SELECTED = 'error-selected';
 
-export function createErrorList(container, trigger) {
-  return new ErrorList(container, trigger);
+export function createErrorList(target, trigger) {
+  return new ErrorList(target, trigger);
 }
 
 const DESKTOP_WIDTH = 1024;
 
-class ErrorList {
-  constructor(container, trigger) {
-    this.container = container;
+class ErrorList extends FlyIn {
+  constructor(target, trigger) {
+    super(target);
+
+    this.target = target;
     this.trigger = Button.from(trigger, this.toggle.bind(this));
+
     // configure validator
     events.subscribe(
       Validator.EVENT_NEW_VALIDATION_RESULT,
@@ -61,73 +65,52 @@ class ErrorList {
     this.validationResult = validationResult;
     window.requestIdleCallback(() => {
       /* eslint-disable max-len */
-      this.container.innerHTML = `
-        <div class="title">
-        <button class="button close">
-          <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
-          </svg>
-        </button>
-        <h2>Validation Result</h2>
-        </div>
-        <ul>${validationResult.errors.map(this.renderError).join('')}</ul>
-        `;
+      const content = document.createElement('ul');
+      for (var i = 0; i < validationResult.errors.length; i++) {
+        const error = validationResult.errors[i]
+        content.appendChild(this.renderError(error, i))
+      }
+
       /* eslint-enable max-len */
       if (validationResult.errors.length === 0) {
-        this.container.classList.toggle('show', false);
+        this.hideFlyIn();
       }
+
+      this.upadateContent(content);
     });
   }
 
   renderError(error, index) {
-    return `<li class="validation-error ${error.icon}" data-index="${index}">
-            <div>
-              <div class="message">${error.message}</div>
-              <div class="category">${error.category}</div>
-              <div class="location">line ${error.line}, column ${error.col}</div>
-            </div>
-      </li>`;
+    const errorElement = document.createElement('li');
+    errorElement.className = `validation-error ${error.icon}`;
+    errorElement.setAttribute('data-index', `${index}`);
+
+    errorElement.innerHTML = (
+      `<div>
+        <p class="message">${error.message}</p>
+        <div class="location">line ${error.line}, column ${error.col}</div>
+      </div>`
+    );
+
+    errorElement.addEventListener('click', (e) => {
+      this.onErrorItemClick(e)
+    });
+
+    return errorElement;
   }
 
-  toggle(e) {
-    if (this.container.classList.contains('show')) {
-      this.hideErrorList(e);
-    } else {
-      this.showErrorList(e);
-    }
-  }
-
-  hideErrorList() {
-    document.body.removeEventListener('click', this.onItemClickHandler, false);
-    this.container.classList.toggle('show', false);
-  }
-
-  showErrorList(e) {
-    e.stopPropagation();
-    this.onItemClickHandler = this.onItemClick.bind(this);
-    document.body.addEventListener('click', this.onItemClickHandler, false);
-    this.container.classList.toggle('show', true);
-  }
-
-  onItemClick(e) {
-    if (!this.validationResult) {
-      return;
-    }
+  onErrorItemClick(e) {
     const target = e.target.closest('li.validation-error');
     if (!target) {
       this.hideErrorList();
       return;
     }
+
     const index = target.dataset.index;
     const error = this.validationResult.errors[index];
-    if (!error) {
-      return;
-    }
-    e.stopPropagation();
-    if (window.innerWidth < DESKTOP_WIDTH) {
-      this.hideErrorList();
-    }
+
     events.publish(EVENT_ERROR_SELECTED, error);
   }
+
+
 }
---
+++ playground/src/fly-in/base.js
@@ -0,0 +1,76 @@
+// Copyright 2018 The AMPHTML Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+require('./fly-in.scss');
+
+class FlyIn {
+  constructor(target) {
+    const content = document.createElement('div')
+    content.className = 'fly-in-container';
+    this.content = content;
+
+    this.createFlyIn(target);
+
+    this.registerButtons(target);
+  }
+
+  createFlyIn(target) {
+    const title = target.getAttribute('data-title');
+    target.innerHTML = `
+      <div class="fly-in-header">
+        <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+          <path d="M0 0h24v24H0z" fill="none"></path>
+        </svg>
+        <h2>${title}</h2>
+        <div class="fly-in-close" tabindex="0" role="button">
+          <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+            <path d="M0 0h24v24H0z" fill="none"></path>
+          </svg>
+        </div>
+      </div>
+    `;
+
+    target.appendChild(this.content);
+
+    return target;
+  }
+
+  upadateContent(content) {
+    this.content.innerHTML = '';
+    this.content.appendChild(content);
+  }
+
+  registerButtons(target) {
+    const button = target.querySelector('.fly-in-close');
+    button.addEventListener('click', () => {
+      this.hideFlyIn(target);
+    });
+  }
+
+  toggle() {
+    this.target.classList.toggle('active');
+  }
+
+  hideFlyIn() {
+    this.target.classList.remove('active');
+  }
+
+  showFlyIn() {
+    this.target.classList.add('active');
+  }
+}
+
+export default FlyIn;
