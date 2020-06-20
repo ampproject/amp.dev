ba8e403843fa132b1f72a9a6f82040495265bc67
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-06-20 14:36:50 +0200

    Enable cors for playground dev server

--- playground/development.js
+++ playground/development.js
@@ -17,8 +17,10 @@
 require('module-alias/register');
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use('/', require('./backend/'));
 
 const port = 8080;
