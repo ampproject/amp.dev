271785c1df3d92d9ef23a637156680efc68678ff
Matthias Rohmer <matthias.rohmer@jvm.de>
2020-07-01 16:15:37 +0200

    🐛 Fix import of Google Analytics tracking ID

--- playground/src/analytics/index.js
+++ playground/src/analytics/index.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import gaTrackingId from '../../../platform/config/shared.json';
+import {gaTrackingId} from '../../../platform/config/shared.json';
 import modes from '../modes/';
 
 class Analytics {
