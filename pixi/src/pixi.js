c60d8a009aff0f31b58e9ec4175a1470c4c3b0df
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-08-13 11:14:14 +0200

    🚨 Fix linter warnings.

--- pixi/src/pixi.js
+++ pixi/src/pixi.js
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import button from './button/button.js';
+import _ from './button/button.js';
