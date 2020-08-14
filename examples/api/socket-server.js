52bef5c29460fd692a19304f3f751b550062c6dd
Ben Morss <morss@google.com>
2020-08-14 12:02:49 -0400

    Committing not working socket server to get help

--- examples/api/socket-server.js
+++ examples/api/socket-server.js
@@ -24,6 +24,18 @@
 'use strict';
 
 const WebSocket = require('ws');
+const express = require('express');
+
+// eslint-disable-next-line new-cap
+const examples = express.Router();
+/* We probably don't need this, do we? Since we're actually adding the socket server in platform.js?
+ * The problem I have is that if I try to export a function like startLiveBlog below,
+ * some sort of magic process wants to include that in express anyway, throwing an error.
+ * But I was wondering if I still needed to create a Router and then do something like this:
+ * examples.put('/live-blog', function(req, res) {
+ *   // server logic here
+ * }
+*/
 
 const messages = [
   "Something thrilling is happening.",
