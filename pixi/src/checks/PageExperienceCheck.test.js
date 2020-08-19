86abe9f813940e9fcd82ee36dfaab602ade6297d
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-08-19 15:05:36 +0200

    🎨 Use fetch-mock instead of jest.spyOn

--- pixi/src/checks/PageExperienceCheck.test.js
+++ pixi/src/checks/PageExperienceCheck.test.js
@@ -6,14 +6,16 @@ import PageExperienceCheck from './PageExperienceCheck.js';
 import dummyApiResponse from '../../mocks/pageExperienceCheck/apiResponse.json';
 import expectedReportData from '../../mocks/pageExperienceCheck/reportData.json';
 
+import fetchMock from 'fetch-mock';
+
 beforeAll(() => {
-  jest.spyOn(PageExperienceCheck.prototype, 'fetchJson').mockImplementation(() => {
-    return dummyApiResponse
-  });
+  fetchMock.reset();
 });
 
 describe('Page experience check', () => {
   it('creates report data for url', async () => {
+    fetchMock.mock('*', dummyApiResponse);
+
     const pageExperienceCheck = new PageExperienceCheck();
     const reportData = await pageExperienceCheck.run('https://example.com/');
     expect(reportData).toMatchObject(expectedReportData);
