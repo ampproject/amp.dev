45dfb3debc1e23a0a4487c1d830aa176d6de76c9
Robin <35134232+robinvanopstal@users.noreply.github.com>
2020-08-19 19:48:06 +0200

    ✅  Add tests for PageExperienceCheck.js (#4313)
    * 🚧 Add dummy test
    
    * 🚧 Add dummy test
    
    * ♻️ Remove unused code
    
    * 🐛 Fix dummy experience check test
    
    * 👌 externalize dummy response
    
    * 💡 Update describe block
    
    * 🎨 Use fetch-mock instead of jest.spyOn
    
    * 🎨 Match real API endpoint in fetch-mock
    
    * 🚨 Fix linter warnings.
    
    Co-authored-by: Matthias Rohmer <rohmer.matthias@anyvent.org>

---
+++ pixi/src/checks/PageExperienceCheck.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import fetchMock from 'fetch-mock';
+
+import PageExperienceCheck from './PageExperienceCheck.js';
+import dummyApiResponse from '../../mocks/pageExperienceCheck/apiResponse.json';
+import expectedReportData from '../../mocks/pageExperienceCheck/reportData.json';
+
+import pixiConfig from '../../config.js';
+
+beforeAll(() => {
+  fetchMock.reset();
+});
+
+describe('Page experience check', () => {
+  it('creates report data for url', async () => {
+    const apiEndpoint =
+      pixiConfig['development'].API_ENDPOINT_PAGE_SPEED_INSIGHTS;
+    fetchMock.mock(`begin:${apiEndpoint}`, dummyApiResponse);
+
+    const pageExperienceCheck = new PageExperienceCheck();
+    const reportData = await pageExperienceCheck.run('http://example.com');
+    expect(reportData).toMatchObject(expectedReportData);
+  });
+});
