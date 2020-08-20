9cdd99a338de44df212077f365842329f260b1ad
Matthias Rohmer <12857772+matthiasrohmer@users.noreply.github.com>
2020-08-20 09:42:35 +0200

    🐛  Fix various loose ends for Pixi (#4322)
    * 🐛 Correctly require apiResponse for Pixi backend
    
    * 🐛 Use wildcard to copy Pixi source maps
    
    Co-authored-by: Sebastian Benz <sebastian.benz@gmail.com>

--- pixi/backend/api.js
+++ pixi/backend/api.js
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 const {
   dummyApiResponse,
 } = require('../mocks/pageExperienceCheck/apiResponse.json');
+
 const express = require('express');
 const {lint, LintMode} = require('@ampproject/toolbox-linter');
 const cheerio = require('cheerio');
