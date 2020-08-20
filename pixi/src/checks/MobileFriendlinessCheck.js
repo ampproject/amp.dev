a7191912644431aab7a011b51511c20e0030cdc8
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-08-20 15:18:43 +0200

    ✨ Implement Mobile Friendliness Check including tests

---
+++ pixi/src/checks/MobileFriendlinessCheck.js
@@ -0,0 +1,62 @@
+// Copyright 2020 The AMPHTML Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const API_ENDPOINT = API_ENDPOINT_MOBILE_FRIENDLINESS;
+const API_KEY = process.env.AMP_DEV_API_KEY_MOBILE_FRIENDLINESS;
+
+const API_URL = `${API_ENDPOINT}?key=${API_KEY}`;
+
+export default class MobileFriendlinessCheck {
+  async run(pageUrl) {
+    try {
+      const apiResult = await this.fetchJson(pageUrl);
+      return this.createReportData(null, apiResult);
+    } catch (e) {
+      return this.createReportData(e);
+    }
+  }
+
+  createReportData(error, apiResult) {
+    if (error || apiResult.testStatus.status !== 'COMPLETE') {
+      return {error, data: false};
+    }
+
+    return {error, data: apiResult.mobileFriendliness == 'MOBILE_FRIENDLY'};
+  }
+
+  async fetchJson(pageUrl) {
+    try {
+      const response = await fetch(API_URL, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          url: pageUrl
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `MobileFriendlinessCheck failed: response failed with status ${response.status}`
+        );
+      }
+      const result = await response.json();
+      return result;
+    } catch (e) {
+      throw new Error('MobileFriendlinessCheck failed:', e);
+    }
+  }
+}
