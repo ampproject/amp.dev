5a926e1c8393a8f972bbd64e94c7715c2fd87cfe
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-08-21 10:31:06 +0200

    👽 Adapt CoreWebVitalsReportView to new partials

--- pixi/src/ui/report/CoreWebVitalsReportView.js
+++ pixi/src/ui/report/CoreWebVitalsReportView.js
@@ -13,25 +13,56 @@
 // limitations under the License.
 
 export default class CoreWebVitalsReport {
-  constructor(doc, id) {
-    this.container = doc.getElementById(id);
+  constructor(container) {
+    this.container = container;
     this.label = this.container.querySelector('label');
-    this.indicator = this.container.querySelector('aside');
-    this.indicatorLabel = this.indicator.querySelector('span');
+
+    this.scale = {
+      bar: {
+        positive: this.container.querySelector('.ap-a-pixi-scale-bar-positive'),
+        negative: this.container.querySelector('.ap-a-pixi-scale-bar-negative'),
+      },
+      pitch: {
+        positive: this.container.querySelector('.ap-a-pixi-scale-pitch-line-positive'),
+        negative: this.container.querySelector('.ap-a-pixi-scale-pitch-line-negative')
+      },
+      indicator: this.container.querySelector('.ap-a-pixi-scale-indicator'),
+      value: this.container.querySelector('.ap-a-pixi-scale-value'),
+    }
   }
 
   render(report) {
     const unit = report.unit;
     const data = report.data;
 
+    // Update bar to match distributions
+    this.scale.bar.positive.style.transform = `scale3d(${ data.distributions[0].proportion}, 1, 1)`;
+    this.scale.bar.negative.style.transform = `scale3d(${ data.distributions[2].proportion}, 1, 1)`;
+
+    // Update pitch lines
+    this.scale.pitch.positive.style.transform = `translateX(${data.distributions[0].proportion * 100}%)`;
+    this.scale.pitch.positive.textContent = `${data.distributions[0].max / unit.conversion}${unit.name}`;
+
+    this.scale.pitch.negative.style.transform = `translateX(${100 - data.distributions[2].proportion * 100}%)`;
+    this.scale.pitch.negative.textContent = `${data.distributions[2].min / unit.conversion}${unit.name}`;
+
+    // Add a value to the scale and position in distributions
+    this.scale.value.textContent = `${data.percentile / unit.conversion}${unit.name}`;
+    let distributionOffset = 0;
+    for (const distribution of data.distributions) {
+      if (data.percentile < distribution.max) {
+        this.scale.value.style.width = `${data.percentile / distribution.max * 100}%`;
+        break;
+      }
+
+      distributionOffset += distribution.proportion * 100;
+    }
+    this.scale.indicator.style.marginLeft = `${distributionOffset}%`;
+
     this.container.classList.add(data.category.toLowerCase());
     this.label.textContent = data.category;
 
-    const indicatorX = data.percentile / data.distributions[2].min;
-    this.indicator.style.transform = `translateX(${indicatorX * 100}%)`;
-    this.indicatorLabel.textContent = `${data.percentile / unit.conversion} ${
-      unit.name
-    }`;
+    this.toggleLoading(false);
   }
 
   toggleLoading(force) {
