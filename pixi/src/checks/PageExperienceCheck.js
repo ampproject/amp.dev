418f3617e09f9462811f68934c028a821b02ea8e
Robin Van Opstal <robin.van-opstal@jvm.de>
2020-09-04 17:38:58 +0200

    🐛 Fix broken merge

--- pixi/src/checks/PageExperienceCheck.js
+++ pixi/src/checks/PageExperienceCheck.js
@@ -107,32 +107,35 @@ export default class PageExperienceCheck {
     return metrics[checkId].data.category === Category.FAST;
   }
 
-  createReportData(apiResult) {
-    const fieldMetrics = apiResult.loadingExperience.metrics;
-    const audits = apiResult.lighthouseResult.audits;
+  createReportData(apiResultOrigin, apiResultCache) {
+    const fieldMetricsOrigin = apiResultOrigin.loadingExperience.metrics;
+    const auditsOrigin = apiResultOrigin.lighthouseResult.audits;
 
-    const fieldData = !fieldMetrics
+    const fieldMetricsCache = apiResultCache.loadingExperience.metrics;
+    const auditsCache = apiResultCache.lighthouseResult.audits;
+
+    const fieldData = !fieldMetricsOrigin
       ? undefined
       : {
           lcp: {
             unit: UNIT_SEC,
             data: this.createFieldData(
-              fieldMetrics['LARGEST_CONTENTFUL_PAINT_MS'],
-              'lcp'
+              fieldMetricsOrigin['LARGEST_CONTENTFUL_PAINT_MS'],
+              fieldMetricsCache['LARGEST_CONTENTFUL_PAINT_MS']
             ),
           },
           fid: {
             unit: UNIT_MS,
             data: this.createFieldData(
-              fieldMetrics['FIRST_INPUT_DELAY_MS'],
-              'fid'
+              fieldMetricsOrigin['FIRST_INPUT_DELAY_MS'],
+              fieldMetricsCache['FIRST_INPUT_DELAY_MS']
             ),
           },
           cls: {
             unit: UNIT_DEC,
             data: this.createFieldData(
-              fieldMetrics['CUMULATIVE_LAYOUT_SHIFT_SCORE'],
-              'cls'
+              fieldMetricsOrigin['CUMULATIVE_LAYOUT_SHIFT_SCORE'],
+              fieldMetricsCache['CUMULATIVE_LAYOUT_SHIFT_SCORE']
             ),
           },
         };
@@ -140,15 +143,24 @@ export default class PageExperienceCheck {
     const labData = {
       lcp: {
         unit: UNIT_SEC,
-        data: this.createLabData(audits['largest-contentful-paint'], 'lcp'),
+        data: this.createLabData(
+          auditsOrigin['largest-contentful-paint'],
+          auditsCache['largest-contentful-paint'],
+          'lcp'),
       },
       fid: {
         unit: UNIT_MS,
-        data: this.createLabData(audits['total-blocking-time'], 'fid'),
+        data: this.createLabData(
+          auditsOrigin['total-blocking-time'],
+          auditsCache['total-blocking-time'],
+          'fid'),
       },
       cls: {
         unit: UNIT_DEC,
-        data: this.createLabData(audits['cumulative-layout-shift'], 'cls'),
+        data: this.createLabData(
+          auditsOrigin['cumulative-layout-shift'],
+          auditsCache['cumulative-layout-shift'],
+          'cls'),
       },
     };
 
@@ -165,16 +177,16 @@ export default class PageExperienceCheck {
       labData,
       isAllFast,
       textCompression:
-        this.getAuditScore(audits, 'uses-text-compression') === 1,
+        this.getAuditScore(auditsOrigin, 'uses-text-compression') === 1,
       fastServerResponse:
-        this.getAuditScore(audits, 'server-response-time') === 1,
+        this.getAuditScore(auditsOrigin, 'server-response-time') === 1,
       usesAppropriatelySizedImages:
-        this.getAuditScore(audits, 'uses-responsive-images') === 1,
+        this.getAuditScore(auditsOrigin, 'uses-responsive-images') === 1,
       usesOptimizedImages:
-        this.getAuditScore(audits, 'uses-optimized-images') === 1,
-      usesWebpImages: this.getAuditScore(audits, 'uses-webp-images') === 1,
-      fastFontDisplay: this.getAuditScore(audits, 'font-display') === 1,
-      minifiedCss: this.getAuditScore(audits, 'unminified-css') === 1,
+        this.getAuditScore(auditsOrigin, 'uses-optimized-images') === 1,
+      usesWebpImages: this.getAuditScore(auditsOrigin, 'uses-webp-images') === 1,
+      fastFontDisplay: this.getAuditScore(auditsOrigin, 'font-display') === 1,
+      minifiedCss: this.getAuditScore(auditsOrigin, 'unminified-css') === 1,
     };
 
     return {
