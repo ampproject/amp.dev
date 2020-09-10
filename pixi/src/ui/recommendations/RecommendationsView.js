374fcd65390a227f8e7a7414a62c1e8f9b5f0b1e
Robin <35134232+robinvanopstal@users.noreply.github.com>
2020-09-10 13:18:39 +0200

    ✨ Pixi - Recommendation filter (#4449)
    * 🚧 Setup markup structure
    
    * 🚧 Implement filter logic
    
    * 🚧 Event listener
    
    * 🚨 Fix lint warnings
    
    * ✨ Add filter functionality
    
    * ✨ Add arrow down + scroll to Take action label
    
    * 🎨 Prepare filter function to be accessible from metric
    
    * 🚨 Fix lint warnings
    
    * ✨ Add logic for reset filter
    
    * 👌 Improve code readability
    
    * 👌 Make pill a button
    
    * 🐛 Fix all button class
    
    * ♿️ Add aria label
    
    * 🐛 Only iterate through a unique set of tags
    
    * 🐛 Reset filter on resetView

--- pixi/src/ui/recommendations/RecommendationsView.js
+++ pixi/src/ui/recommendations/RecommendationsView.js
@@ -18,10 +18,16 @@ import i18n from '../I18n.js';
 export default class RecommendationsView {
   constructor(doc) {
     this.container = doc.getElementById('recommendations');
-    this.template = this.container.querySelector(
+
+    this.filter = this.container.querySelector(
+      '.ap-o-pixi-recommendations-filter'
+    );
+    this.pill = this.filter.querySelector('.ap-a-pill');
+
+    this.recommendation = this.container.querySelector(
       '.ap-m-pixi-recommendations-item'
     );
-    this.tag = this.container.querySelector('span');
+    this.tag = this.recommendation.querySelector('span');
   }
 
   resetView() {
@@ -32,13 +38,22 @@ export default class RecommendationsView {
     for (let i = 1; i < items.length; i++) {
       this.container.removeChild(items[i]);
     }
+
+    for (let i = 1; i < this.filter.children.length; i++) {
+      this.filter.removeChild(this.filterPills[i]);
+    }
   }
 
   render(recommendationIds) {
     this.container.classList.remove('pristine');
     const recommendations = i18n.getSortedRecommendations(recommendationIds);
+    const tagIds = [];
+
+    this.recommendationNodes = [];
+    this.filterPills = [];
+
     for (const value of recommendations) {
-      const recommendation = this.template.cloneNode(true);
+      const recommendation = this.recommendation.cloneNode(true);
       const header = recommendation.querySelector(
         '.ap-m-pixi-recommendations-item-header'
       );
@@ -65,11 +80,66 @@ export default class RecommendationsView {
 
       for (const tagId of value.tags) {
         const tag = this.tag.cloneNode(true);
+        recommendation.classList.add(tagId);
         tag.textContent = i18n.getText(`tags.${tagId}`);
         tagsBar.appendChild(tag);
+        tagIds.push(tagId);
       }
 
+      this.recommendationNodes.push(recommendation);
       this.container.appendChild(recommendation);
+    }
+
+    for (const tagId of new Set(tagIds).values()) {
+      const pill = this.pill.cloneNode(true);
+      pill.textContent = i18n.getText(`tags.${tagId}`);
+      pill.id = `filter-pill-${tagId}`;
+      pill.className = 'ap-a-pill';
+
+      pill.addEventListener('click', () => {
+        this.toggleFilter(tagId);
+      });
+
+      this.filterPills.push(pill);
+      this.filter.appendChild(pill);
+    }
+
+    this.pill.classList.add('filtered');
+    this.pill.addEventListener('click', () => {
+      this.resetFilter();
+    });
+  }
+
+  resetFilter() {
+    this.pill.classList.add('filtered');
+    this.container.className = this.container.className.split(' ')[0];
+
+    for (const pill of this.filterPills) {
+      pill.classList.remove('filtered');
+      for (const recommendation of this.recommendationNodes) {
+        recommendation.hidden = false;
+      }
+    }
+  }
+
+  toggleFilter(tagId) {
+    this.container.classList.toggle(tagId);
+    this.pill.classList.remove('filtered');
+    const pill = this.container.querySelector(`#filter-pill-${tagId}`);
+    pill.classList.toggle('filtered');
+
+    const activeFilter = this.container.className.split(' ');
+    if (activeFilter.length == 1) {
+      this.resetFilter();
+      return;
+    }
+
+    for (const recommendation of this.recommendationNodes) {
+      const commonValues = activeFilter.filter((value) => {
+        return recommendation.classList.contains(value);
+      });
+
+      recommendation.hidden = !commonValues.length;
     }
   }
 }
