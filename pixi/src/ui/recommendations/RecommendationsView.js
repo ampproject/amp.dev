125d0832cc8fcec904b325db3cc79af515a64612
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2020-09-15 04:16:38 -0400

    Pixi: Label major regions with accessibility attributes (#4531)
    * Fix accessibility of recommendations accordions
    
    * Aria label toggle buttons
    
    * Label field and lab data sections with role="tab"

--- pixi/src/ui/recommendations/RecommendationsView.js
+++ pixi/src/ui/recommendations/RecommendationsView.js
@@ -70,11 +70,15 @@ export default class RecommendationsView {
 
       recommendation.style = null;
       recommendation.id = `recommendation-${value.id}`;
+      header.id = `header-${value.id}`;
+      body.id = `body-${value.id}`;
 
-      header.setAttribute(
-        'on',
-        `tap:recommendation-${value.id}.toggleClass(class=expanded)`
-      );
+      header.addEventListener('click', () => {
+        const isExpanded = recommendation.classList.toggle('expanded');
+        header.setAttribute('aria-expanded', String(isExpanded));
+      });
+      header.setAttribute('aria-controls', body.id);
+      body.setAttribute('aria-labelledby', header.id);
 
       title.innerHTML = marked(value.title);
       body.innerHTML = marked(value.body);
@@ -97,6 +101,7 @@ export default class RecommendationsView {
       pill.textContent = i18n.getText(`tags.${tagId}`);
       pill.id = `filter-pill-${tagId}`;
       pill.className = 'ap-a-pill';
+      pill.setAttribute('aria-pressed', 'false');
 
       pill.addEventListener('click', () => {
         this.toggleFilter(tagId);
@@ -121,10 +126,12 @@ export default class RecommendationsView {
 
   resetFilter() {
     this.pill.classList.add('filtered');
+    pill.setAttribute('aria-pressed', 'true');
     this.container.className = this.container.className.split(' ')[0];
 
     for (const pill of this.filterPills) {
       pill.classList.remove('filtered');
+      pill.setAttribute('aria-pressed', 'false');
       for (const recommendation of this.recommendationNodes) {
         recommendation.hidden = false;
       }
@@ -134,8 +141,10 @@ export default class RecommendationsView {
   toggleFilter(tagId) {
     this.container.classList.toggle(tagId);
     this.pill.classList.remove('filtered');
+    this.pill.setAttribute('aria-pressed', 'false');
     const pill = this.container.querySelector(`#filter-pill-${tagId}`);
-    pill.classList.toggle('filtered');
+    const isFiltered = pill.classList.toggle('filtered');
+    pill.setAttribute('aria-pressed', isFiltered);
 
     const activeFilter = this.container.className.split(' ');
     if (activeFilter.length == 1) {
