30aae670d13b2b222918f8b04e44d25336e5c46c
Matthias Rohmer <12857772+matthiasrohmer@users.noreply.github.com>
2020-09-22 22:09:12 +0200

    ♻️ Stop replacing HTML entities in Pixi (#4649)
    * 📌 Pin AMP to 002009190410000 runtime version
    
    * ♻️  Stop replacing HTML entities in Pixi
    
    * 💡 Update issue hints in comments
    
    * ✅ Remove unneeded test case

--- pixi/src/utils/texts.test.js
+++ pixi/src/utils/texts.test.js
@@ -1,11 +1,6 @@
 import {cleanCodeForInnerHtml, addTargetBlankToLinks} from './texts';
 
 describe('cleanCodeForInnerHtml', () => {
-  it('converts specific html entities', async () => {
-    const result = cleanCodeForInnerHtml('&#39; &quot; &amp; &lt; &gt;');
-    expect(result.trim()).toEqual('\' " \uFF06 \uFE64 \uFE65');
-  });
-
   it('removes empty span tags', async () => {
     const result = cleanCodeForInnerHtml('<pre><span> </span></pre>');
     expect(result.trim()).toEqual('<pre></pre>');
