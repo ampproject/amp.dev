3251957553e06b0301f546675a65ac0455ccd28d
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-09-23 15:31:58 +0200

    🐛 Correctly anchor next advise below fixed header

--- pixi/src/ui/recommendations/RecommendationsView.js
+++ pixi/src/ui/recommendations/RecommendationsView.js
@@ -104,7 +104,7 @@ export default class RecommendationsView {
       }
 
       recommendation.style = null;
-      recommendation.id = `recommendation-${value.id}`;
+      title.id = `recommendation-${value.id}`;
       header.id = `header-${value.id}`;
       body.id = `body-${value.id}`;
 
