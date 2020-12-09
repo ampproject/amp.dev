8b6231c9ccfcb15e60d7fa28bb964f8cfb41df35
Ben Zörb <ben@sommerlaune.com>
2020-12-09 14:20:02 +0100

    Consider missing loadingExperience in api result (#5044)
    fixes #5040

--- pixi/src/checks/PageExperienceCheck.test.js
+++ pixi/src/checks/PageExperienceCheck.test.js
@@ -8,6 +8,8 @@ import fetchMock from 'fetch-mock';
 import PageExperienceCheck from './PageExperienceCheck.js';
 import apiResponseNoErrors from '../../mocks/pageExperienceCheck/apiResponse.json';
 import reportDataNoErrors from '../../mocks/pageExperienceCheck/reportData.json';
+import apiResponseNoLoadingExperience from '../../mocks/pageExperienceCheck/apiResponseNoLoadingExperience.json';
+import reportDataNoLoadingExperience from '../../mocks/pageExperienceCheck/reportDataNoLoadingExperience.json';
 import apiResponseErrors from '../../mocks/pageExperienceCheck/apiResponseErrors.json';
 import reportDataErrors from '../../mocks/pageExperienceCheck/reportDataErrors.json';
 import apiResponseNoFieldData from '../../mocks/pageExperienceCheck/apiResponseNoFieldData.json';
@@ -56,6 +58,18 @@ describe('Page experience check', () => {
     const report = await pageExperienceCheck.run('http://example.com');
     expect(report.error).toBeUndefined();
     expect(report.data).toEqual(reportDataNoFieldData);
+    expect(report.descriptions).toEqual(reportDescriptions);
+  });
+
+  it('creates report data for url with no loadingExperience', async () => {
+    const apiEndpoint =
+      pixiConfig['development'].API_ENDPOINT_PAGE_SPEED_INSIGHTS;
+    fetchMock.mock(`begin:${apiEndpoint}`, apiResponseNoLoadingExperience);
+
+    const pageExperienceCheck = new PageExperienceCheck();
+    const report = await pageExperienceCheck.run('http://example.com');
+    expect(report.error).toBeUndefined();
+    expect(report.data).toEqual(reportDataNoLoadingExperience);
     expect(report.descriptions).toEqual(reportDescriptions);
   });
 
