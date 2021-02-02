15d2e274a88d0410f02cfe24671be28c67341ed0
Leo Luerich <leo.luerich@jvm.de>
2021-02-02 11:35:55 +0100

    :package: Update extension due to grow update

--- pages/extensions/log_beautifier/log_beautifier.py
+++ pages/extensions/log_beautifier/log_beautifier.py
@@ -1,7 +1,7 @@
 import logging
 
 from grow import extensions
-from grow.common import logger
+from grow.common import logger, colors
 from grow.server import manager
 
 logger._get_logger = logger.get_logger
@@ -29,9 +29,22 @@ manager._print_server_ready_message = manager.print_server_ready_message
 def print_server_ready_message(pod, host, port):
     home_doc = pod.get_home_doc()
     root_path = home_doc.url.path if home_doc and home_doc.exists else '/'
-    url = 'http://{}:{}{}'.format(host, port, root_path)
+    url_base = 'http://{}:{}/'.format(host, port)
+    url_root = 'http://{}:{}{}'.format(host, port, root_path)
     logger.LOGGER.info('Grow started successfully.')
-    return url
+
+    messages = manager.ServerMessages()
+    messages.add_message('Pod:', pod.root, colors.HIGHLIGHT)
+    messages.add_message('Server:', url_root, colors.HIGHLIGHT)
+
+    # Trigger the dev manager message hook.
+    extra_urls = pod.extensions_controller.trigger(
+        'dev_manager_message', messages.add_message, url_base, url_root) or []
+
+    messages.add_message(
+        'Ready.', 'Press ctrl-c to quit.', colors.SUCCESS, colors.SUCCESS)
+
+    return (url_root, extra_urls)
 
 manager.print_server_ready_message = print_server_ready_message
 
