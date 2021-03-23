dcec079a6e54be2c0e84642f14ebc13bf833c0db
Sebastian Benz <sbenz@google.com>
2021-03-23 09:22:26 +0100

    extract var replacement into config

--- pixi/rollup.config.js
+++ pixi/rollup.config.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 The AMPHTML Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
@@ -5,7 +20,6 @@ import commonjs from '@rollup/plugin-commonjs';
 const config = require('./config.js');
 
 const mode = process.argv.mode || 'production';
-const apiKey = process.env.AMP_DEV_PIXI_APIS_KEY;
 
 export default {
   input: 'src/Cli.js',
@@ -13,25 +27,5 @@ export default {
     dir: 'dist',
     format: 'cjs',
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    replace({
-      IS_DEVELOPMENT: mode !== 'production',
-      API_ENDPOINT_LINTER: JSON.stringify(config[mode].API_ENDPOINT_LINTER),
-      API_ENDPOINT_LINTER_CANARY: JSON.stringify(
-        config[mode].API_ENDPOINT_LINTER_CANARY
-      ),
-      API_ENDPOINT_SAFE_BROWSING: JSON.stringify(
-        config[mode].API_ENDPOINT_SAFE_BROWSING
-      ),
-      API_ENDPOINT_PAGE_SPEED_INSIGHTS: JSON.stringify(
-        config[mode].API_ENDPOINT_PAGE_SPEED_INSIGHTS
-      ),
-      API_ENDPOINT_MOBILE_FRIENDLINESS: JSON.stringify(
-        config[mode].API_ENDPOINT_MOBILE_FRIENDLINESS
-      ),
-      AMP_DEV_PIXI_APIS_KEY: JSON.stringify(apiKey),
-    }),
-  ],
+  plugins: [resolve(), commonjs(), replace(config.createKeyMapping(mode))],
 };
