30ec41a1824ccbd2d6f81fcea105d4a876bd7a63
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-07-13 14:01:39 +0200

    🚸 Do not throw errors for users without 404 page

--- platform/lib/routers/notFound.js
+++ platform/lib/routers/notFound.js
@@ -16,23 +16,30 @@
 
 'use strict';
 
-const {pagePath} = require('@lib/utils/project');
-const {setMaxAge} = require('@lib/utils/cacheHelpers.js');
-const {optimizer} = require('@lib/utils/ampOptimizer.js');
-const {readFileSync} = require('fs');
-const {join} = require('path');
+const { pagePath } = require('@lib/utils/project');
+const { setMaxAge } = require('@lib/utils/cacheHelpers.js');
+const { optimizer } = require('@lib/utils/ampOptimizer.js');
+const { readFileSync } = require('fs');
+const { join } = require('path');
 
-let optimized;
+let optimizedPage;
+
+async function getNotFoundPage() {
+  if (!optimizedPage) {
+    try {
+      const page = readFileSync(join(pagePath(), '404.html'), 'utf-8');
+      optimizedPage = await optimizer.transformHtml(page);
+    } catch (e) {
+      optimizedPage = 'The page you\'ve requested can\'t be found.';
+    }
+  }
+  return optimizedPage;
+}
 
 module.exports = async (req, res) => {
   setMaxAge(res, 60 * 10); // ten minutes
   if (req.headers.accept && req.headers.accept.includes('text/html')) {
-    if (!optimized) {
-      const FourOhFour = readFileSync(join(pagePath(), '404.html'), 'utf-8');
-      optimized = await optimizer.transformHtml(FourOhFour);
-    }
-
-    res.status(404).send(optimized);
+    res.status(404).send(getNotFoundPage());
     return;
   }
 
