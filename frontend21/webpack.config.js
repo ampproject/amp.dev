edba198ee0e0117df6c651d7ba7daffc04e757ae
lluerich <lluerich@users.noreply.github.com>
2021-09-13 14:49:55 +0200

    🔧 Update webpack-dev-server config (#6113)
    * :wrench: Update devServer config
    
    See [changelog](https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md)
    
    * :bug: Remove redundant import

--- frontend21/webpack.config.js
+++ frontend21/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -98,7 +97,6 @@ module.exports = (env, argv) => {
           },
         },
       }),
-      new webpack.HotModuleReplacementPlugin({}),
       isDevelopment
         ? new WebpackBuildNotifierPlugin({
             title: 'amp.dev: Frontend',
@@ -175,15 +173,17 @@ module.exports = (env, argv) => {
     },
 
     devServer: {
-      overlay: true,
-      contentBase: '/',
-      writeToDisk: true,
-      disableHostCheck: true,
+      static: {
+        directory: '/',
+      },
+      devMiddleware: {
+        writeToDisk: true,
+      },
+      allowedHosts: 'all',
       port: 8090,
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
-      compress: true,
       hot: true,
     },
   };
