f52122c765b26f5d2db2eadc012f9738e56e5913
Sebastian Benz <sbenz@google.com>
2022-05-31 15:47:43 +0200

    upgrade to node v12

--- pixi/cloud-function/deploy.sh
+++ pixi/cloud-function/deploy.sh
@@ -1,1 +1,1 @@
-gcloud functions deploy checkPageExperience --runtime nodejs10 --trigger-http --allow-unauthenticated --project amp-dev-230314 
+gcloud functions deploy checkPageExperience --runtime nodejs12 --trigger-http --allow-unauthenticated --project amp-dev-230314 
