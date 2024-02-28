670e93f6293062b820660eb1508dd3026afc7883
Daniel Rozenberg <rodaniel@amp.dev>
2024-02-28 11:54:16 -0500

    Rewrite deploy code

--- gulpfile.js/deploy-static.js
+++ gulpfile.js/deploy-static.js
@@ -17,35 +17,73 @@
 'use strict';
 
 const {sh} = require('@lib/utils/sh.js');
+const {cyan, red} = require('ansi-colors');
 const {DIST, PAGES_DEST} = require('@lib/utils/project').paths;
-const {NETLIFY_DEPLOY_TOKEN} = process.env;
-const SITES = [
-  {
-    NAME: 'amp.dev',
-    ID: 'e571c70e-d23f-4cbf-ac4e-802bb08e5261',
-    DIR: PAGES_DEST,
-  },
-  {
-    NAME: 'playground.amp.dev',
-    ID: 'acead270-9404-4dde-81e4-aec0e6884869',
-    DIR: `${DIST}/playground`,
-  },
-  {
-    NAME: 'preview.amp.dev',
-    ID: 'caf28d42-024a-4efb-b266-b00cf10847a3',
-    DIR: `${DIST}/examples`,
-  },
-];
+const {NETLIFY_DEPLOY_TOKEN, DEPLOY_ENVIRONMENT} = process.env;
+
+/** @typedef {'Staging' | 'Production'} DeploymentEnvironmentsDef */
+/** @typedef {{name: string, id: string, dir: string}} DeploymentSiteDef */
+/** @typedef {Record<DeploymentEnvironmentsDef, DeploymentSiteDef[]>} DeploymentConfigDef */
+
+// Which deploy environment to use if the env variable is not set.
+const DEFAULT_DEPLOY_ENVIRONMENT = 'Staging';
+
+/** @type {DeploymentConfigDef} */
+const SITES = {
+  'Staging': [
+    {
+      name: 'staging-amp-dev.netlify.app',
+      id: '75115d1b-a53a-4e0c-ac21-93930d604d0b',
+      dir: PAGES_DEST,
+    },
+    {
+      name: 'playground-staging-amp-dev.netlify.app',
+      id: 'a9f7ee74-0f49-49ff-9c2d-5cac85df3dc8',
+      dir: `${DIST}/playground`,
+    },
+    {
+      name: 'preview-staging-amp-dev.netlify.app',
+      id: '8ade0d74-9b2c-4e8e-8d85-73e198208fba',
+      dir: `${DIST}/examples`,
+    },
+  ],
+  'Production': [
+    {
+      name: 'amp.dev',
+      id: 'e571c70e-d23f-4cbf-ac4e-802bb08e5261',
+      dir: PAGES_DEST,
+    },
+    {
+      name: 'playground.amp.dev',
+      id: 'acead270-9404-4dde-81e4-aec0e6884869',
+      dir: `${DIST}/playground`,
+    },
+    {
+      name: 'preview.amp.dev',
+      id: 'caf28d42-024a-4efb-b266-b00cf10847a3',
+      dir: `${DIST}/examples`,
+    },
+  ],
+};
 
 async function staticDeploy() {
-  for (const SITE of SITES) {
-    console.log(`attempting to deploy ${SITE.DIR}`);
+  const deployEnvironment = DEPLOY_ENVIRONMENT ?? DEFAULT_DEPLOY_ENVIRONMENT;
+  if (!(deployEnvironment in SITES)) {
+    console.error(
+      red('Deploy environment'),
+      cyan(deployEnvironment),
+      red('was specified but is not a valid environment')
+    );
+    console.info('Valid environments are:', Object.keys(SITES));
+    process.exit(1);
+  }
+
+  for (const {name, id, dir} of SITES[deployEnvironment]) {
+    console.log('Deploying', cyan(dir), 'to', cyan(name));
 
     await sh(
-      `npx netlify deploy --prod --auth ${NETLIFY_DEPLOY_TOKEN} --site ${SITE.ID}`,
-      {
-        workingDir: SITE.DIR,
-      }
+      `npx netlify deploy --prod --auth ${NETLIFY_DEPLOY_TOKEN} --dir ${dir} --site ${id}`,
+      {workingDir: dir}
     );
   }
 }
