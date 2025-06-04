7a4aeb9d29a6c2aa934b1e7686b4accb32d21830
powerivq <poweriv.q@gmail.com>
2025-06-04 13:55:54 -0700

    ALso sanitize dots and parenthesises for slug generation

--- pages/extensions/markdown_toc_patch/custom_toc_tree_processor.py
+++ pages/extensions/markdown_toc_patch/custom_toc_tree_processor.py
@@ -1,11 +1,26 @@
+import re
+import unicodedata
 from markdown import util
 from markdown.extensions.toc import TocExtension
 from markdown.extensions.toc import TocTreeprocessor
+
+
+def slugify(value: str, separator: str = '-', unicode: bool = False) -> str:
+  """ Slugify a string, to make it URL friendly. """
+  slug = value.strip().lower()
+  slug = slug.replace(' ', '-')
+  slug = re.sub(r'[^\w-]', '', slug, flags=re.UNICODE)
+  return slug
+
 
 class CustomTocTreeProcessor(TocTreeprocessor):
   """
   Since html code that may be inside the headlines causes problems in the TOC we remove them
   """
+
+  def __init__(self, md, config):
+    super(CustomTocTreeProcessor, self).__init__(md, config)
+    self.slugify = slugify
 
   def strip_html_from_names(self, toc_list):
     for item in toc_list:
